368e5edb8a669d2f396c20938bed1edd01c0bd89
Richard Uhler <ruhler@csail.mit.edu>
2015-03-05 21:36:16 -0800

    Wrote a draft of the new Function/Expr interface.
    It looks nice. Now I just have to implement it.

--- androcles/function.h
+++ androcles/function.h
@@ -1,21 +1,74 @@
 
-#ifndef FUNCTION_H_
-#define FUNCTION_H_
+#ifndef ANDROCLES_FUNCTION_H_
+#define ANDROCLES_FUNCTION_H_
 
-struct VDecl {
-  const Type* type;
-  const std::string name;
-  const Expr* value;
-}
+#include <string>
+#include <vector>
 
-class Function {
+#include "androcles/type.h"
+#include "androcles/value.h"
+
+class Expr_;
+
+class Expr {
+ public:
+  Type GetType() const;
+
  private:
-  const std::string name_;
-  const std::vector<Arg> args_;
-  const Type* outtype_;
-  const std::vector<VDecl> vdecls_;
-  const Expr* return_;
+  Expr(const Expr_* expr);
+  const Expr_* expr_;
 };
 
-#endif//FUNCTION_H_
+struct Alt {
+  std::string tag;
+  Expr value;
+};
+
+class Function {
+ public:
+  Function(const std::string& name, 
+      const std::vector<Field>& args,
+      Type out_type);
+  Function::~Function();
+
+  // Returns an expression referring to the variable with the given name.
+  // The name must match either an input variable or a declared variable.
+  Expr Var(const std::string& name);
+
+  // Declare a variable with the given type, name, and value.
+  // Returns an Expr referring to the declared variable.
+  Expr Declare(Type type, const std::string& name, Expr expr);
+
+  // Returns a union literal expression.
+  Expr Union(Type type, const std::string& field_name, Expr value);
+
+  // Returns a struct literal expression.
+  Expr Struct(Type type, const std::vector<Expr> args);
+
+  // Returns a select expression.
+  Expr Select(Expr select, const std::vector<Alt>& alts);
+
+  // Returns a field access expression.
+  Expr Access(Expr source, const std::string& field_name);
+
+  // Returns a function application expression.
+  Expr Application(Expr function, const std::vector<Expr>& args);
+
+  // Defines the result of the function as the given expression.
+  void Return(Expr expr);
+
+  // Evaluate the function on the given arguments.
+  Value Eval(const std::vector<Value>& args) const;
+
+ private:
+  const std::string name_;
+  const std::vector<Field> fields_;
+  Type out_type_;
+  Expr return_;
+
+  // Ownership for all expressions created through this function.
+  std::vector<std::unique_ptr<Expr_>> exprs_;
+};
+
+#endif//ANDROCLES_FUNCTION_H_
 
---
+++ androcles/function_test.cc
@@ -0,0 +1,53 @@
+
+#include <memory>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "androcles/function.h"
+#include "androcles/type.h"
+#include "androcles/value.h"
+
+TEST(AndroclesFunctionTest, Basic) {
+  TypeEnv type_env;
+
+  Type unit_t = type_env.DeclareStruct("Unit", {});
+  Type bit_t = type_env.DeclareUnion("Bit", {{unit_t, "0"}, {unit_t, "1"}});
+  Type full_adder_out_t = type_env.DeclareStruct("FullAdderOut", {
+      {bit_t, "z"},
+      {bit_t, "cout"}
+  });
+
+  Function adder("FullAdder",
+      {{bit_t, "a"}, {bit_t, "b"}, {bit_t, "cin"}},
+      full_adder_out_t);
+
+  Expr a = adder.Var("a");
+  Expr b = adder.Var("b");
+  Expr cin = adder.Var("cin");
+  Expr b0 = adder.Declare(bit_t, "0", adder.Union(bit_t, "0", adder.Struct(unit_t, {})));
+  Expr b1 = adder.Declare(bit_t, "1", adder.Union(bit_t, "1", adder.Struct(unit_t, {})));
+  Expr z = adder.Declare(
+      adder.Select(a, {
+        {"0", adder.Select(b, {
+                {"0", cin},
+                {"1", adder.Select(cin, {{"0", b1}, {"1", b0}})}})},
+        {"1", adder.Select(b, {
+                {"0", adder.Select(cin, {{"0", b1}, {"1", b0}})},
+                {"1", cin}})}));
+  Expr cout = adder.Declare(
+      adder.Select(a, {
+        {"0", adder.Select(b, {{"0", b0}, {"1", cin}})},
+        {"1", adder.Select(b, {{"0", cin}, {"1", b1}})}));
+  adder.Return(adder.Struct(full_adder_out_t, {z, cout}));
+
+  Value unit_v = Value::Struct(unit_t, {});
+  Value b0_v = Value::Union(bit_t, "0", unit_v);
+  Value b1_v = Value::Union(bit_t, "1", unit_v);
+
+  EXPECT_EQ(Value::Struct(full_adder_out_t, {b1_v, b0_v}),
+      full_adder.Eval({b0_v, b1_v, b0_v}));
+  EXPECT_EQ(Value::Struct(full_adder_out_t, {b0_v, b1_v}),
+      full_adder.Eval({b0_v, b1_v, &b1_v}));
+}
+
