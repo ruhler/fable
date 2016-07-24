20836d867b589c8a63cf8e0f5b254341c4481615
Richard Uhler <ruhler@degralder.com>
2016-07-24 15:09:42 -0700

    Replace --expect-error with better exit codes.
    So the test harness can detect the proper error occurred itself.

--- build.tcl
+++ build.tcl
@@ -25,12 +25,21 @@ foreach {x} [lsort [glob test/????v-*.fblc]] {
 foreach {x} [lsort [glob test/????e-*.fblc]] {
   puts "test $x"
   set fgot out/[string map {.fblc .got} [file tail $x]]
-  exec ./out/fblc --expect-error $x 2> $fgot
+  try {
+    exec ./out/fblc $x 2> $fgot
+    error "Expected exit code 65, but got 0"
+  } trap CHILDSTATUS {results options} {
+    set status [lindex [dict get $options -errorcode] 2]
+    if {$status != 65} {
+      error "Expected exit code 65, but got $status"
+    }
+  }
 }
 
-exec gcov {*}[glob out/*.o] > out/fblc.gcov
+# Report how much code coverage we have from the spec tests.
+exec gcov {*}[glob out/*.o] > out/fblc.spectest.gcov
 exec mv {*}[glob *.gcov] out
-puts [exec tail -n 1 out/fblc.gcov]
+puts [exec tail -n 1 out/fblc.spectest.gcov]
 
 # Test the calculator.
 puts ""
@@ -39,6 +48,7 @@ exec ./out/fblc calc/calc.fblc > out/calc.got
 exec grep "/// Expect: " calc/calc.fblc | sed -e "s/\\/\\/\\/ Expect: //" > out/calc.wnt
 exec diff out/calc.wnt out/calc.got
 
+# Report the final test coverage stats.
 exec gcov {*}[glob out/*.o] > out/fblc.gcov
 exec mv {*}[glob *.gcov] out
 puts [exec tail -n 1 out/fblc.gcov]
--- fblc/FblcMain.c
+++ fblc/FblcMain.c
@@ -4,6 +4,11 @@
 
 #include "FblcInternal.h"
 
+#define EX_OK 0
+#define EX_USAGE 64
+#define EX_DATAERR 65
+#define EX_NOINPUT 66
+
 static void PrintUsage(FILE* fout);
 
 // PrintUsage --
@@ -22,14 +27,9 @@ static void PrintUsage(FILE* fout);
 static void PrintUsage(FILE* stream)
 {
   fprintf(stream,
-      "Usage: fblc [--expect-error] FILE\n"
+      "Usage: fblc FILE\n"
       "Evaluate 'main()' in the environment of the fblc program FILE.\n"  
       "Example: fblc foo.fblc\n"
-      "\n"
-      "Options:\n"
-      "   --expect-error\n"
-      "     If present, fblc exits with code 0 for malformed input programs,\n"
-      "     and non-zero otherwise.\n"
   );
 }
 
@@ -57,18 +57,15 @@ int main(int argc, char* argv[])
   MALLOC_INIT();
 
   const char* filename = NULL;
-  bool expect_error = false;
   for (int i = 1; i < argc; i++) {
     if (strcmp("--help", argv[i]) == 0) {
       PrintUsage(stdout);
-      return 0;
-    } else if (strcmp("--expect-error", argv[i]) == 0) {
-      expect_error = true;
+      return EX_OK;
     } else {
       if (filename != NULL) {
         fprintf(stderr, "multiple FILEs are not allowed.\n");
         PrintUsage(stderr);
-        return 1;
+        return EX_USAGE;
       }
       filename = argv[i];
     }
@@ -76,13 +73,13 @@ int main(int argc, char* argv[])
 
   if (filename == NULL) {
     fprintf(stderr, "no input file.\n");
-    return 1;
+    return EX_USAGE;
   }
 
   FblcTokenStream toks;
   if (!FblcOpenTokenStream(&toks, filename)) {
     fprintf(stderr, "failed to open input FILE %s.\n", filename);
-    return 1;
+    return EX_NOINPUT;
   }
 
   FblcAllocator alloc;
@@ -91,19 +88,22 @@ int main(int argc, char* argv[])
   FblcCloseTokenStream(&toks);
   if (env == NULL) {
     fprintf(stderr, "failed to parse input FILE.\n");
-    return expect_error ? 0 : 1;
+    FblcFreeAll(&alloc);
+    return EX_DATAERR;
   }
 
   if (!FblcCheckProgram(env)) {
     fprintf(stderr, "input FILE is not a well formed Fblc program.\n");
-    return expect_error ? 0 : 1;
+    FblcFreeAll(&alloc);
+    return EX_DATAERR;
   }
 
   FblcFunc* func = FblcLookupFunc(env, "main");
   if (func != NULL) {
     if (func->argc != 0) {
       fprintf(stderr, "main function does not take 0 arguments.\n");
-      return 1;
+      FblcFreeAll(&alloc);
+      return EX_USAGE;
     }
 
     FblcValue* value = FblcEvaluate(env, func->body);
@@ -112,19 +112,21 @@ int main(int argc, char* argv[])
     FblcRelease(value);
     FblcFreeAll(&alloc);
     CHECK_FOR_LEAKS();
-    return expect_error ? 1 : 0;
+    return EX_OK;
   }
 
   FblcProc* proc = FblcLookupProc(env, "main");
   if (proc != NULL) {
     if (proc->portc != 0) {
+      FblcFreeAll(&alloc);
       fprintf(stderr, "main process does not have 0 ports.\n");
-      return 1;
+      return EX_USAGE;
     }
 
     if (proc->argc != 0) {
+      FblcFreeAll(&alloc);
       fprintf(stderr, "main process does not take 0 arguments.\n");
-      return 1;
+      return EX_USAGE;
     }
 
     FblcValue* value = FblcExecute(env, proc->body);
@@ -133,9 +135,10 @@ int main(int argc, char* argv[])
     FblcRelease(value);
     FblcFreeAll(&alloc);
     CHECK_FOR_LEAKS();
-    return expect_error ? 1 : 0;
+    return EX_OK;
   }
 
+  FblcFreeAll(&alloc);
   fprintf(stderr, "failed to find 'main' function.\n");
-  return 1;
+  return EX_USAGE;
 }
