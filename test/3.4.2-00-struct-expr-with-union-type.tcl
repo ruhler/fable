58b53f44fb343569a3be5a6afe025c4fb64e20cd
Richard Uhler <ruhler@degralder.com>
2016-11-13 10:50:34 -0800

    Move struct with union type test to new format.

---
+++ test/3.4.2-00-struct-expr-with-union-type.tcl
@@ -0,0 +1,14 @@
+
+# A struct expression can't be used for a union type.
+
+set prg {
+  struct Unit();
+  union Bool(Unit True, Unit False);
+
+  func main( ; Bool) {
+    Bool(Unit(), Unit());
+  };
+}
+
+expect_malformed $prg main
+
