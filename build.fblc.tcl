af95df140de6c14db1495998d0a4d4467ecfbd4c
Richard Uhler <ruhler@degralder.com>
2017-06-24 10:12:40 +0100

    Separate fblc and fbld build scripts.
    So it is easier to build just fblc if desired.

---
+++ build.fblc.tcl
@@ -0,0 +1,100 @@
+
+exec rm -rf out
+set FLAGS [list -I fblc -std=c99 -pedantic -Wall -Werror -O0 -fprofile-arcs -ftest-coverage -gdwarf-3 -ggdb] 
+
+# Compile all object files.
+# We compile these separately and ensure they are placed in a subdirectory of
+# the out directory so that the profile information generated when running
+# code from the objects is placed in a subdirectory of the out directory.
+exec mkdir -p out/fblc/obj
+foreach {x} [glob fblc/*.c] {
+  set obj out/fblc/obj/[string map {.c .o} [file tail $x]]
+  run gcc {*}$FLAGS -c -o $obj $x
+}
+
+# Generate libfblc.a
+set fblc_objs [list]
+foreach {x} [list check exec fblcs load malloc parse resolve value vector] {
+  lappend fblc_objs out/fblc/obj/$x.o
+}
+run ar rcs out/fblc/libfblc.a {*}$fblc_objs
+
+# Compile the executables
+set ::fblc ./out/fblc/fblc
+set ::fblccheck ./out/fblc/fblc-check
+set ::fblctest ./out/fblc/fblc-test
+run gcc {*}$FLAGS -o $::fblc out/fblc/obj/fblc.o -L out/fblc -lfblc
+run gcc {*}$FLAGS -o $::fblccheck out/fblc/obj/fblc-check.o -L out/fblc -lfblc
+run gcc {*}$FLAGS -o $::fblctest out/fblc/obj/fblc-test.o -L out/fblc -lfblc
+run gcc {*}$FLAGS -o out/fblc/fblc-snake out/fblc/obj/fblc-snake.o -L out/fblc -lfblc -lncurses
+run gcc {*}$FLAGS -o out/fblc/fblc-tictactoe out/fblc/obj/fblc-tictactoe.o -L out/fblc -lfblc
+
+# See langs/fblc/README.txt for the description of this function
+proc fblc-check-error { program loc } {
+  set testloc [info frame -1]
+  set line [dict get $testloc line]
+  set file [dict get $testloc file]
+  set name "[file tail $file]_$line"
+
+  exec mkdir -p out/test/fblc
+  set fprogram ./out/test/fblc/$name.fblc
+  exec echo $program > $fprogram
+  try {
+    set errtext [exec $::fblccheck --error $fprogram]
+  } on error {results options} {
+    error "$file:$line: error: fblc-check passed unexpectedly: $results"
+  }
+  exec echo $errtext > ./out/test/fblc/$name.err
+  if {-1 == [string first ":$loc: error" $errtext]} {
+    error "$file:$line: error: Expected error at $loc, but got:\n$errtext"
+  }
+}
+
+# See langs/fblc/README.txt for the description of this function
+proc fblc-test { program entry args script } {
+  set loc [info frame -1]
+  set line [dict get $loc line]
+  set file [dict get $loc file]
+  set name "[file tail $file]_$line"
+
+  # Write the script to file.
+  exec mkdir -p out/test/fblc
+  set fscript ./out/test/fblc/$name.script
+  exec rm -f $fscript
+  exec touch $fscript
+  foreach cmd [split [string trim $script] "\n"] {
+    exec echo [string trim $cmd] >> $fscript
+  }
+
+  # Write the program to file.
+  set fprogram ./out/test/fblc/$name.fblc
+  exec echo $program > $fprogram
+
+  try {
+    exec $::fblctest $fscript $fprogram $entry {*}$args
+  } on error {results options} {
+    error "$file:$line: error: \n$results"
+  }
+}
+
+foreach {x} [lsort [glob langs/fblc/*.tcl]]  {
+  puts "test $x"
+  source $x
+}
+
+exec mkdir -p out/fblc/cov/spec
+run gcov {*}$::fblc_objs > out/fblc/cov/spec/fblc.gcov
+exec mv {*}[glob *.gcov] out/fblc/cov/spec
+
+run $::fblctest prgms/clockincr.wnt prgms/clock.fblc incr "Digit:1(Unit())"
+run $::fblctest prgms/calc.wnt prgms/calc.fblc main
+run $::fblctest prgms/tictactoe.TestBoardStatus.wnt prgms/tictactoe.fblc TestBoardStatus
+run $::fblctest prgms/tictactoe.TestChooseBestMoveNoLose.wnt prgms/tictactoe.fblc TestChooseBestMoveNoLose
+run $::fblctest prgms/tictactoe.TestChooseBestMoveWin.wnt prgms/tictactoe.fblc TestChooseBestMoveWin
+run $::fblctest prgms/boolcalc.wnt prgms/boolcalc.fblc Test
+run $::fblctest prgms/ints.wnt prgms/ints.fblc Test
+run $::fblccheck prgms/snake.fblc
+
+exec mkdir -p out/fblc/cov/all
+run gcov {*}$::fblc_objs > out/fblc/cov/all/fblc.gcov
+exec mv {*}[glob *.gcov] out/fblc/cov/all
---
+++ build.fbld.tcl
@@ -0,0 +1,104 @@
+
+exec rm -rf out/fbld
+set FLAGS [list -I fblc -I fbld -std=c99 -pedantic -Wall -Werror -O0 -fprofile-arcs -ftest-coverage -gdwarf-3 -ggdb] 
+
+# Compile all object files.
+# We compile these separately and ensure they are placed in a subdirectory of
+# the out directory so that the profile information generated when running
+# code from the objects is placed in a subdirectory of the out directory.
+exec mkdir -p out/fbld/obj
+foreach {x} [glob fbld/loc.c fbld/name.c] {
+  set obj out/fbld/obj/[string map {.c .o} [file tail $x]]
+  run gcc {*}$FLAGS -c -o $obj $x
+}
+
+# Generate and compile the fbld parser.
+exec mkdir -p out/fbld/src
+run bison -o out/fbld/src/parse.tab.c fbld/parse.y 
+run gcc {*}$FLAGS -c -o out/fbld/obj/parse.tab.o out/fbld/src/parse.tab.c
+
+# Generate libfbld.a
+set fbld_objs [list]
+#foreach {x} [list check compile decl load loc name parse.tab] {
+foreach {x} [list loc name parse.tab] {
+  lappend fbld_objs out/fbld/obj/$x.o
+}
+run ar rcs out/fbld/libfbld.a {*}$fbld_objs
+
+# Compile the executables
+set ::fbldcheck ./out/fbld/fbld-check
+set ::fbldtest ./out/fbld/fbld-test
+run gcc {*}$FLAGS -o $::fbldcheck out/fbld/obj/fbld-check.o -L out/fbld -lfbld -L out/fblc -lfblc
+run gcc {*}$FLAGS -o $::fbldtest out/fbld/obj/fbld-test.o -L out/fbld -lfbld -L out/fblc -lfblc
+
+# See langs/fbld/README.txt for the description of this function
+proc fbld-check-error { program module loc } {
+  set testloc [info frame -1]
+  set line [dict get $testloc line]
+  set file [dict get $testloc file]
+  set name "[file tail $file]_$line"
+  set dir ./out/test/fbld/$name
+  exec mkdir -p $dir
+
+  # Write the modules to file.
+  foreach {name content} $program {
+    exec echo $content > $dir/$name
+  }
+
+  set errtext "no error reported"
+  try {
+    set errtext [exec $::fbldcheck --error $dir $module]
+  } on error {results options} {
+    error "$file:$line: error: $results"
+  }
+
+  exec echo $errtext > $dir/$name.err
+  if {-1 == [string first "$loc: error" $errtext]} {
+    error "$file:$line: error: Expected error at $loc, but got:\n$errtext"
+  }
+}
+
+# See langs/fbld/README.txt for the description of this function.
+proc fbld-test { program entry args script } {
+  set loc [info frame -1]
+  set line [dict get $loc line]
+  set file [dict get $loc file]
+  set name "[file tail $file]_$line"
+  set dir ./out/test/fbld/$name
+  exec mkdir -p $dir
+
+  # Generate the script.
+  set fscript $dir/script
+  exec rm -f $fscript
+  exec touch $fscript
+  foreach cmd [split [string trim $script] "\n"] {
+    exec echo [string trim $cmd] >> $fscript
+  }
+
+  # Write the modules to file.
+  foreach {name content} $program {
+    exec echo $content > $dir/$name
+  }
+
+  try {
+    exec $::fbldtest $fscript $dir $entry {*}$args
+  } on error {results options} {
+    error "$file:$line: error: \n$results"
+  }
+}
+
+foreach {x} [lsort [glob langs/fbld/*.tcl]]  {
+  puts "test $x"
+  source $x
+}
+
+exec mkdir -p out/fbld/spec
+run gcov {*}$::fbld_objs > out/fbld/cov/spec/fbld.gcov
+exec mv {*}[glob *.gcov] out/fbld/cov/spec
+
+run $::fbldtest prgms/UBNatTest.wnt prgms Test@UBNatTest
+run $::fbldtest prgms/PrimesTest.wnt prgms Test@PrimesTest
+
+exec mkdir -p out/fbld/cov/all
+run gcov {*}$::fbld_objs > out/fbld/cov/all/fbld.gcov
+exec mv {*}[glob *.gcov] out/fbld/cov/all
