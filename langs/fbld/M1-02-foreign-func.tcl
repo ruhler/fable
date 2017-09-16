31db02aacd03e0be95171a05adaec28c1f12bb3e
Richard Uhler <ruhler@degralder.com>
2017-09-16 19:26:06 +0100

    Add test of calling a foreign function.

---
+++ langs/fbld/M1-02-foreign-func.tcl
@@ -0,0 +1,38 @@
+# Test that we can call a foreign function.
+set prg {
+  UnitM.mtype {
+    mtype UnitM<> {
+      struct Unit();
+      func f( ; Unit);
+    };
+  }
+
+  UnitM.mdefn {
+    mdefn UnitM< ; ; UnitM<>> {
+      struct Unit();
+      func f( ; Unit) {
+        Unit();
+      };
+    };
+  }
+
+  Main.mtype {
+    mtype Main<> {
+      using UnitM<;> { Unit; };
+      func main( ; Unit);
+    };
+  }
+
+  Main.mdefn {
+    mdefn Main< ; ; Main<>> {
+      using UnitM<;> { Unit; f; };
+      func main( ; Unit) {
+        f();
+      };
+    };
+  }
+}
+
+skip fbld-test $prg "main@Main<;>" {} {
+  return Unit@UnitM<;>()
+}
