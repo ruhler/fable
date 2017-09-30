00c66ecef556636e594fc0ca20de48033540776a
Richard Uhler <ruhler@degralder.com>
2017-09-30 08:19:39 +0100

    Port 2.3.3* tests from fblc to fbld.

---
+++ langs/fbld/2.3.3-00-union-access.tcl
@@ -0,0 +1,25 @@
+# Access a component of a union.
+set prg {
+  Main.mtype {
+    mtype Main<> {
+      struct Unit();
+      union Fruit(Unit apple, Unit banana, Unit pear);
+    };
+  }
+
+  Main.mdefn {
+    mdefn Main< ; ; Main<>> {
+      struct Unit();
+      union Fruit(Unit apple, Unit banana, Unit pear);
+      union Maybe(Unit nothing, Fruit just);
+
+      func main( ; Fruit) {
+        Maybe:just(Fruit:pear(Unit())).just;
+      };
+    };
+  }
+}
+
+fbld-test $prg "main@Main<;>" {} {
+  return Fruit@Main<;>:pear(Unit@Main<;>())
+}
---
+++ langs/fbld/2.3.3-01-union-access-unknown-field.tcl
@@ -0,0 +1,21 @@
+set prg {
+  Main.mtype {
+    mtype Main<> {
+    };
+  }
+
+  Main.mdefn {
+    mdefn Main< ; ; Main<>> {
+      struct Unit();
+      union Fruit(Unit apple, Unit banana, Unit pear);
+      union Maybe(Unit nothing, Fruit just);
+
+      func main( ; Fruit) {
+        # nosuchfield is not a field of Maybe
+        Maybe:just(Fruit:pear(Unit())).nosuchfield;
+      };
+    };
+  }
+}
+
+fbld-check-error $prg Main Main.mdefn:9:40
