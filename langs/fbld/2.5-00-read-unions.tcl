ae05de2981859764eb7c4861c4201bc22571f69c
Richard Uhler <ruhler@degralder.com>
2017-10-02 07:09:13 +0100

    Ported 2.5-* test from fblc to fbld.

---
+++ langs/fbld/2.5-00-read-unions.tcl
@@ -0,0 +1,28 @@
+set prg {
+  Main.mtype {
+    mtype Main<> {
+      struct Unit();
+      union Bool(Unit True, Unit False);
+      proc main(Bool <~ in ; ; Bool);
+    };
+  }
+
+  Main.mdefn {
+    mdefn Main< ; ; Main<>> {
+      struct Unit();
+      union Bool(Unit True, Unit False);
+
+      proc main(Bool <~ in ; ; Bool) {
+        Bool x = ~in();
+        ~in();
+      };
+    };
+  }
+}
+
+# Verify we can read two union values in a row.
+fbld-test $prg "main@Main<;>" {} {
+  put in Bool@Main<;>:False(Unit@Main<;>())
+  put in Bool@Main<;>:True(Unit@Main<;>())
+  return Bool@Main<;>:True(Unit@Main<;>())
+}
