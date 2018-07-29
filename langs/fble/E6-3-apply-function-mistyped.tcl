fe9d295a5a548114fdc96971eeef25b14695fd9b
Richard Uhler <ruhler@degralder.com>
2018-07-29 16:15:44 +0100

    Add more tests for function application.

---
+++ langs/fble/E6-3-apply-function-mistyped.tcl
@@ -0,0 +1,9 @@
+fble-test-error 8:3 {
+  @ Unit = *();
+  @ Bool = +(Unit true, Unit false);
+  Bool true = Bool@(true: Unit@());
+  Bool false = Bool@(false: Unit@());
+
+  # The function is not a function.
+  Unit@()(false, true);
+}
---
+++ langs/fble/E6-7-apply-too-many-args.tcl
@@ -0,0 +1,13 @@
+fble-test-error 12:3 {
+  @ Unit = *();
+  @ Bool = +(Unit true, Unit false);
+  Bool true = Bool@(true: Unit@());
+  Bool false = Bool@(false: Unit@());
+
+  \(Bool x, Bool y; Bool) Snd = \(Bool x, Bool y) {
+    y;
+  };
+
+  # The function is provided with too many arguments.
+  Snd(false, true, true);
+}
