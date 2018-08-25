496c39a29d9a9d3b49abb68eede968c0ae671c0e
Richard Uhler <ruhler@degralder.com>
2018-08-25 12:09:34 +0100

    Ported struct value tests to new spec.

---
+++ langs/fble/3.2-06-struct-value-mistyped-arg.tcl
@@ -0,0 +1,10 @@
+fble-test-error 9:19 {
+  @ Unit@ = *();
+  @ Bool@ = +(Unit@ true, Unit@ false);
+  Bool@ true = Bool@(true: Unit@());
+  Bool@ false = Bool@(false: Unit@());
+
+  # The struct value's second arg has the wrong type.
+  @ BoolPair@ = *(Bool@ x, Bool@ y);
+  BoolPair@(true, Unit@());
+}
