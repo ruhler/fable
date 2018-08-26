517c82b2f2175bff10bfe6e361b437a91087354d
Richard Uhler <ruhler@degralder.com>
2018-08-26 14:06:49 +0100

    Port func value tests to new spec.

---
+++ langs/fble/5.2-00-func-value.tcl
@@ -0,0 +1,11 @@
+fble-test {
+  @ Unit@ = *();
+  @ Bool@ = +(Unit@ true, Unit@ false);
+  Bool@ true = Bool@(true: Unit@());
+  Bool@ false = Bool@(false: Unit@());
+
+  # Construct a basic function value.
+  \(Bool@ x, Bool@ y) {
+    ?(x; true: true, false: y);
+  };
+}
