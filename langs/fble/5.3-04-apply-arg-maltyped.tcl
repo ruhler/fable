f6a856551c9b1ebfbdae3e6d9e13f99099f322ba
Richard Uhler <ruhler@degralder.com>
2018-08-26 14:26:29 +0100

    Port apply tests to new fble spec.

---
+++ langs/fble/5.3-04-apply-arg-maltyped.tcl
@@ -0,0 +1,13 @@
+fble-test-error 12:14 {
+  @ Unit@ = *();
+  @ Bool@ = +(Unit@ true, Unit@ false);
+  Bool@ true = Bool@(true: Unit@());
+  Bool@ false = Bool@(false: Unit@());
+
+  \(Bool@ x, Bool@ y; Bool@) Snd = \(Bool@ x, Bool@ y) {
+    y;
+  };
+
+  # The second argument to the function fails to compile.
+  Snd(false, zzz);
+}
--- langs/fble/pending/E6-3-apply-function-mistyped.tcl
+++
@@ -1,9 +0,0 @@
-fble-test-error 8:3 {
-  @ Unit = *();
-  @ Bool = +(Unit true, Unit false);
-  Bool true = Bool@(true: Unit@());
-  Bool false = Bool@(false: Unit@());
-
-  # The function is not a function.
-  Unit@()(false, true);
-}
