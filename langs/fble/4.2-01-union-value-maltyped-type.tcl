25a5d355e4a6824cda9ad6585b686816e606f14f
Richard Uhler <ruhler@degralder.com>
2018-08-26 13:49:07 +0100

    Ported union value tests to new spec.

--- langs/fble/4.2-01-union-value-maltyped-type.tcl
+++ langs/fble/4.2-01-union-value-maltyped-type.tcl
@@ -1,10 +1,9 @@
-fble-test-error 9:3 {
+fble-test-error 8:3 {
   @ Unit@ = *();
   @ Bool@ = +(Unit@ true, Unit@ false);
   Bool@ true = Bool@(true: Unit@());
   Bool@ false = Bool@(false: Unit@());
 
   # The union value type fails to compile.
-  @ Maybe@ = +(Bool@ just, Unit@ nothing);
   zzz@(just: true);
 }
