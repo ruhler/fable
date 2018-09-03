570d197f068e571f6db32aa2d3d18ec41eb13755
Richard Uhler <ruhler@degralder.com>
2018-09-03 07:06:53 +0100

    Added (failing) test for recursive functions.

---
+++ langs/fble/5.2-04-func-value-recursive.tcl
@@ -0,0 +1,13 @@
+fble-test {
+  @ Unit@ = *();
+  @ Int@ = +(Unit@ 0, Unit@ 1, Unit@ 2, Unit@ 3);
+
+  # There should be no issue with having zeroify be recursively defined.
+  \(Int@ x; Int@) zeroify = \(Int@ x) {
+    ?(x; 0: x,
+         1: zeroify(Int@(0: Unit@())),
+         2: zeroify(Int@(1: Unit@())),
+         3: zeroify(Int@(2: Unit@())));
+  };
+  zeroify(Int@(3: Unit@())).0;
+}
