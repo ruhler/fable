b274ee0aa63f893e78df5c6e5b66d600997989b1
Richard Uhler <ruhler@degralder.com>
2018-09-14 07:07:19 +0100

    Write a basic test for polymorphism.

---
+++ langs/fble/8.2-00-poly.tcl
@@ -0,0 +1,11 @@
+fble-test {
+  # Test a basic use of polymorphic types.
+  @ Unit@ = *();
+  \<@; @> Maybe@ = \<@ T@> { +(T@ just, Unit@ nothing); };
+
+  @ Bool@ = +(Unit@ true, Unit@ false);
+  Bool@ true = Bool@(true: Unit@());
+
+  Maybe@<Bool@> mb = Maybe@<Bool@>(just: true);
+  mb.just.true;
+}
