899a7695d13953e70aac27088fb353be5e311a96
Richard Uhler <ruhler@degralder.com>
2019-02-09 19:59:10 +0000

    Wrote test for multi-binding exec.

---
+++ langs/fble/6.8-03-exec-multi-binding.tcl
@@ -0,0 +1,12 @@
+fble-test {
+  @ Unit@ = *();
+  @ Bool@ = +(Unit@ true, Unit@ false);
+  Bool@ true = Bool@(true: Unit@());
+  Bool@ false = Bool@(false: Unit@());
+
+  @ Units@ = *(Unit@ a, Unit@ b);
+
+  # Test exec with multiple processes.
+  Bool@ t := $(true), Bool@ f := $(false);
+  $(Units@(t.true, f.false));
+}
