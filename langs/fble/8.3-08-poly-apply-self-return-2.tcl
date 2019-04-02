45255629de39a82ed439cb487ca35e39183a1ef8
Richard Uhler <ruhler@degralder.com>
2019-04-02 07:49:33 +0100

    Add test capturing type error.
    Work around the bug in List.fble.

---
+++ langs/fble/8.3-08-poly-apply-self-return-2.tcl
@@ -0,0 +1,14 @@
+fble-test {
+  @ Unit@ = *();
+
+  <@>@ P@ = <@ T@> { *(T@ head, S@<T@> tail); },
+  <@>@ S@ = <@ T@> { +(P@<T@> cons, Unit@ nil); };
+
+  # Regression test of a poly application that results in itself.
+  # There was a bug previously where this was a type error.
+  S@ S0 = <@ T@> {
+    S@<T@>(nil: Unit@());
+  };
+
+  S0;
+}
