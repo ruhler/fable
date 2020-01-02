bb099c455bcea66f81791fefe46e760ff9751578
Richard Uhler <ruhler@degralder.com>
2020-01-01 21:37:47 -0800

    Change spec for exec to evaluate bindings at process execution.
    Rather than evaluating the bindings when we create the process. Because it's
    not at all intuitive that:
    
      Foo@! x = {
         A@ a := foo(...);
         bar(a);
      };
    
    Will cause foo(...) to be evaluated before executing the process x.
    
    I'm pretty sure this spec change is the right thing to do. Not totally sure
    yet.

---
+++ langs/fble/6.4-proc-exec/eval/strict.tcl
@@ -0,0 +1,13 @@
+fble-test {
+  @ Unit@ = *();
+  @ Maybe@ = +(Unit@! just, Unit@ nothing);
+  Maybe@ nothing = Maybe@(nothing: Unit@());
+
+  # Proc exec is non-strict: the bindings are not evaluated until we run the
+  # process itself.
+  Unit@! mku = {
+    Unit@ u := nothing.just;
+    $(u);
+  };
+  Unit@();
+}
