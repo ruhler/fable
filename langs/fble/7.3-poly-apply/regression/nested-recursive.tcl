9587638c01d717327b33688eaa8762f744bbfaa8
Richard Uhler <ruhler@degralder.com>
2020-01-19 12:28:41 -0800

    Minimize nested-recursive.tcl test case.

--- langs/fble/7.3-poly-apply/regression/nested-recursive.tcl
+++ langs/fble/7.3-poly-apply/regression/nested-recursive.tcl
@@ -1,13 +1,16 @@
 fble-test {
-  @ Unit@ = *();
-
   # TODO: Uncomment the following line to re-enable the test.
   ???
 
   # Regression test for when we had an infinite loop trying to test type
   # equality when doing nested application in a recursively defined poly.
-  <@>@ Map@ = <@ T@>{ +(Unit@ empty, MapP@<T@> map); },
-  <@>@ MapP@ = <@ T@>{ *(Unit@ unit, Map@<Map@<T@>> product); };
+
+  # Here's the real motivating example:
+  #<@>@ Map@ = <@ T@>{ +(Unit@ empty, MapP@<T@> map); },
+  #<@>@ MapP@ = <@ T@>{ *(Unit@ unit, Map@<Map@<T@>> product); };
+
+  # Here's a minimized example:
+  <@>@ M@ = <@ T@>{ *(M@<M@<T@>> m); };
   
-  Unit@();
+  *();
 }
