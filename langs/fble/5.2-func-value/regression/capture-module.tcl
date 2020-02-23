87342d18d77e3c0412602ae921641051745c2e58
Richard Uhler <ruhler@degralder.com>
2020-02-23 09:57:23 -0800

    Add regression test for module ref capture.

---
+++ langs/fble/5.2-func-value/regression/capture-module.tcl
@@ -0,0 +1,19 @@
+fble-test {
+  { /Bool%; @(Unit@, Bool@); };
+
+  # Regression test for a bug we had where we were not properly capturing
+  # module refs in function values.
+  (Unit@) { Bool@; } f = (Unit@ _) {
+    /Bool%.True;
+  };
+
+  f(Unit@()).true;
+} {
+  Bool {
+    @ Unit@ = *();
+    @ Bool@ = +(Unit@ true, Unit@ false);
+    Bool@ True = Bool@(true: Unit@());
+    Bool@ False = Bool@(false: Unit@());
+    @(Unit@, Bool@, True, False);
+  } {}
+}
