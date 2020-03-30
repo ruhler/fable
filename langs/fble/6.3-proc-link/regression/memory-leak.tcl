4812007c11e2761202be55c26d452b92e54b239e
Richard Uhler <ruhler@degralder.com>
2020-03-29 17:34:02 -0700

    Found memory leak in link values.
    Not sure how to fix it yet.

---
+++ langs/fble/6.3-proc-link/regression/memory-leak.tcl
@@ -0,0 +1,16 @@
+fble-test {
+  # Regression test where we were not properly tracking references for links,
+  # so that a link that was in a reference cycle with itself would never be
+  # freed.
+  @ Unit@ = *();
+
+  Unit@! ~ get, put;
+  Unit@ _ := put({
+    # Put a Unit@! on the link that references the get port, which itself
+    # references the link, to create a reference cycle on the link with
+    # itself.
+    Unit@! x := get;
+    x;
+  });
+  $(Unit@());
+}
