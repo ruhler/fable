b95ec3749821bc72a3fdf5af751424a7f013a75f
Richard Uhler <ruhler@degralder.com>
2021-03-17 20:29:08 -0700

    Add fble prgms executables to build.ninja.

--- build.ninja.tcl
+++ build.ninja.tcl
@@ -18,7 +18,7 @@ rule build_ninja
 cFlags = -std=c99 -pedantic -Wall -Werror -gdwarf-3 -ggdb
 
 rule obj
-  command = gcc $cFlags -I $idir -c -o $out $in
+  command = gcc $cFlags $iflags -c -o $out $in
   
 rule parser
   command = bison --report=all --report-file=$report -o $tab_c $in
@@ -27,7 +27,7 @@ rule lib
   command = ar rcs $out $in
 
 rule exe
-  command = gcc $cFlags -L $libdir -o $out $in $libs
+  command = gcc $cFlags $lflags -o $out $in $libs
 
 rule copy
   command = cp $in $out
@@ -43,7 +43,7 @@ foreach {x} [glob fble/*.c] {
   lappend fble_objs $object
   # TODO: Use gcc to generate better header dependencies.
   puts "build $object: obj $x | $internal_headers"
-  puts "  idir = fble"
+  puts "  iflags = -I fble"
 }
 
 # TODO: Use gcc to generate better header dependencies somehow?
@@ -53,7 +53,7 @@ puts "  report = ninja/src/parse.tab.report.txt"
 
 # TODO: Use gcc to generate better header dependencies somehow?
 puts "build $obj/parse.tab.o: obj ninja/src/parse.tab.c | $internal_headers"
-puts "  idir = fble"
+puts "  iflags = -I fble"
 lappend fble_objs $obj/parse.tab.o
 
 puts "build ninja/lib/libfble: lib $fble_objs"
@@ -64,12 +64,18 @@ foreach {x} [glob fble/fble*.h] {
   puts "build ninja/include/[file tail $x]: copy $x"
 }
 
-foreach {x} [glob tools/*.c] {
+foreach {x} [glob tools/*.c prgms/fble-md5.c prgms/fble-stdio.c] {
   set base [file rootname [file tail $x]]
-  puts "build ninja/obj/$base.o: obj tools/$base.c | $public_headers"
-  puts "  idir = ninja/include"
+  puts "build ninja/obj/$base.o: obj $x | $public_headers"
+  puts "  iflags = -I ninja/include"
   puts "build ninja/bin/$base: exe ninja/obj/$base.o | ninja/lib/libfble"
-  puts "  libdir = ninja/lib"
+  puts "  lflags = -L ninja/lib"
   puts "  libs = -lfble"
 }
+
+puts "build ninja/obj/fble-app.o: obj prgms/fble-app.c | $public_headers"
+puts "  iflags = -I ninja/include -I /usr/include/SDL2"
+puts "build ninja/bin/fble-app: exe ninja/obj/fble-app.o | ninja/lib/libfble"
+puts "  lflags = -L ninja/lib"
+puts "  libs = -lfble -lSDL2"
 
