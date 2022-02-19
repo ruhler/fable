ce9c32c0b4276f152fed8bca5761e0675a831af1
Richard Uhler <ruhler@degralder.com>
2022-02-19 10:59:54 -0800

    Run /Invaders/Tests% separate from /Fble/Tests%.

--- build.ninja.tcl
+++ build.ninja.tcl
@@ -608,6 +608,16 @@ eval {
   pkg invaders ""
   app $::out/pkgs/invaders/Invaders/fble-invaders "/Invaders/App%" \
     "fble-invaders" $::out/pkgs/invaders/libfble-invaders.a
+
+  # /Invaders/Tests% interpreted
+  test $::out/pkgs/invaders/Invaders/tests.tr "$::out/pkgs/core/Core/fble-stdio $::out/pkgs/invaders/Invaders/Tests.fble.d" \
+    "$::out/pkgs/core/Core/fble-stdio [exec pkg-config --cflags-only-I fble-invaders] -m /Invaders/Tests%" "pool = console"
+
+  # /Invaders/Tests% compiled
+  stdio $::out/pkgs/invaders/Invaders/invaders-tests "/Invaders/Tests%" "fble-invaders" \
+    "$::out/pkgs/core/libfble-core.a $::out/pkgs/app/libfble-app.a $::out/pkgs/invaders/libfble-invaders.a"
+  test $::out/pkgs/invaders/Invaders/invaders-tests.tr $::out/pkgs/invaders/Invaders/invaders-tests \
+    "$::out/pkgs/invaders/Invaders/invaders-tests" "pool = console"
 }
 
 # pinball package
