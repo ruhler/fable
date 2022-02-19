e11b43875f744e3ae80d9b3144d78a9f6539b6f1
Richard Uhler <ruhler@degralder.com>
2022-02-19 11:34:48 -0800

    Separate /Games/Tests% from /Fble/Tests%.

--- build.ninja.tcl
+++ build.ninja.tcl
@@ -601,7 +601,20 @@ eval {
     "$::out/pkgs/hwdg/Hwdg/hwdg-tests" "pool = console"
 }
 
-pkg games ""
+# games package
+eval {
+  pkg games ""
+
+  # /Games/Tests% interpreted
+  test $::out/pkgs/games/Games/tests.tr "$::out/pkgs/core/Core/fble-stdio $::out/pkgs/games/Games/Tests.fble.d" \
+    "$::out/pkgs/core/Core/fble-stdio [exec pkg-config --cflags-only-I fble-games] -m /Games/Tests%" "pool = console"
+
+  # /Games/Tests% compiled
+  stdio $::out/pkgs/games/Games/games-tests "/Games/Tests%" "fble-games" \
+    "$::out/pkgs/core/libfble-core.a $::out/pkgs/app/libfble-app.a $::out/pkgs/games/libfble-games.a"
+  test $::out/pkgs/games/Games/games-tests.tr $::out/pkgs/games/Games/games-tests \
+    "$::out/pkgs/games/Games/games-tests" "pool = console"
+}
 
 # invaders package
 eval {
