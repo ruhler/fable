8786a378a8aea94367818c182adb38b574ac430d
Richard Uhler <ruhler@degralder.com>
2022-02-19 09:39:02 -0800

    Run /Hwdg/Tests% separate from /Fble/Tests%.

--- build.ninja.tcl
+++ build.ninja.tcl
@@ -586,7 +586,21 @@ eval {
     "$::out/pkgs/sat/Sat/sat-tests" "pool = console"
 }
 
-pkg hwdg ""
+# hwdg package
+eval {
+  pkg hwdg ""
+
+  # /Hwdg/Tests% interpreted
+  test $::out/pkgs/hwdg/Hwdg/tests.tr "$::out/pkgs/core/Core/fble-stdio $::out/pkgs/hwdg/Hwdg/Tests.fble.d" \
+    "$::out/pkgs/core/Core/fble-stdio [exec pkg-config --cflags-only-I fble-hwdg] -m /Hwdg/Tests%" "pool = console"
+
+  # /Hwdg/Tests% compiled
+  stdio $::out/pkgs/hwdg/Hwdg/hwdg-tests "/Hwdg/Tests%" "fble-hwdg" \
+    "$::out/pkgs/core/libfble-core.a $::out/pkgs/hwdg/libfble-hwdg.a"
+  test $::out/pkgs/hwdg/Hwdg/hwdg-tests.tr $::out/pkgs/hwdg/Hwdg/hwdg-tests \
+    "$::out/pkgs/hwdg/Hwdg/hwdg-tests" "pool = console"
+}
+
 pkg games ""
 
 # invaders package
