0d65d8741423a5eed3571e979cc65aad48be36d0
Richard Uhler <ruhler@degralder.com>
2022-02-22 07:51:15 -0800

    Remove use of pkg-config for md5 package build.

--- pkgs/md5/build.tcl
+++ pkgs/md5/build.tcl
@@ -1,17 +1,16 @@
 namespace eval "pkgs/md5" {
-  set objs [list]
 
   # .c library files.
+  set objs [list]
   foreach {x} { Md5/md5.fble } {
     lappend objs $::out/pkgs/md5/$x.o
     obj $::out/pkgs/md5/$x.o pkgs/md5/$x.c "-I fble/include -I pkgs/core -I pkgs/md5"
   }
-
   pkg md5 [list core] $objs
 
   # Md5/Tests interpreted
   test $::out/pkgs/md5/Md5/tests.tr "$::out/pkgs/core/Core/fble-stdio $::out/pkgs/md5/Md5/Tests.fble.d" \
-    "$::out/pkgs/core/Core/fble-stdio [exec pkg-config --cflags-only-I fble-md5] -m /Md5/Tests%" "pool = console"
+    "$::out/pkgs/core/Core/fble-stdio -I pkgs/core -I pkgs/md5 -m /Md5/Tests%" "pool = console"
 
   # Md5/Tests compiled
   stdio $::out/pkgs/md5/Md5/md5-tests "/Md5/Tests%" "fble-md5" \
@@ -21,10 +20,12 @@ namespace eval "pkgs/md5" {
 
   # fble-md5 program.
   obj $::out/pkgs/md5/Md5/fble-md5.o pkgs/md5/Md5/fble-md5.c \
-    [exec pkg-config --cflags fble fble-core fble-md5]
-  bin $::out/pkgs/md5/Md5/fble-md5 "$::out/pkgs/md5/Md5/fble-md5.o" \
-    [exec pkg-config --static --libs fble-md5] \
-    "$::out/fble/lib/libfble.a $::out/pkgs/core/libfble-core.a $::out/pkgs/md5/libfble-md5.a"
+    "-I fble/include -I pkgs/core -I pkgs/md5"
+  set objs "$::out/pkgs/md5/Md5/fble-md5.o"
+  append objs " $::out/pkgs/md5/libfble-md5.a"
+  append objs " $::out/pkgs/core/libfble-core.a"
+  append objs " $::out/fble/lib/libfble.a"
+  bin $::out/pkgs/md5/Md5/fble-md5 $objs ""
 
   # fble-md5 test
   test $::out/pkgs/md5/Md5/fble-md5.tr "$::out/pkgs/md5/Md5/fble-md5 $::out/pkgs/md5/Md5/Main.fble.d" \
