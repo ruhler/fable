2bac441fcd85d92d127e8d4b4bad14b0bba4b762
Richard Uhler <ruhler@degralder.com>
2022-03-03 07:47:52 -0800

    Start draft code for compiling in fble-spec-test.tcl.
    So far it just parses the .d file.

--- fble/test/fble-spec-test.tcl
+++ fble/test/fble-spec-test.tcl
@@ -14,7 +14,7 @@
 # appropriately. Reports an error in case of test failure.
 #
 # This script assumes it is run from the top level directory, with access to
-# fble binaries in the out directory and out/${FBLE}.d the result of running
+# fble binaries in the out directory and out/spec/${FBLE}.d the result of running
 # fble-deps on the FBLE file.
 
 set ::dir "spec"
@@ -45,10 +45,26 @@ while {[gets $fin line] >= 0} {
 }
 close $fin
 
+proc compile {} {
+  # Parse the list of .fble files the test depends on from the .d file.
+  set fbles [list]
+  foreach x [lrange [split [read [open out/spec/$::fble.d "r"]]] 1 end] {
+    switch $x {
+      {} {}
+      "\\" {}
+      default { lappend fbles [string map { "spec/" "" } $x] }
+    }
+  }
+
+  # TODO: compile each of the .fble files and link into an executable.
+  puts $fbles
+}
+
 proc dispatch {} {
   switch $::type {
     no-error {
       # TODO: Run compiled too.
+      compile
       exec out/fble/test/fble-test.cov --profile /dev/null -I spec -m $::mpath
       exec out/fble/bin/fble-disassemble.cov -I spec -m $::mpath
     }
@@ -62,6 +78,7 @@ proc dispatch {} {
 
     runtime-error {
       # TODO: Run compiled too.
+      compile
       set output [exec out/fble/test/fble-test.cov --runtime-error -I spec -m $::mpath]
       if {-1 == [string first ":$::loc: error" $output]} {
         error "Expected error at $::loc, but got:\n$output"
@@ -71,12 +88,14 @@ proc dispatch {} {
     
     memory-constant {
       # TODO: Run compiled too.
+      compile
       exec out/fble/test/fble-mem-test.cov -I spec -m $::mpath
       exec out/fble/bin/fble-disassemble.cov -I spec -m $::mpath
     }
 
     memory-growth {
       # TODO: Run compiled too.
+      compile
       exec out/fble/test/fble-mem-test.cov --growth -I spec -m $::mpath
       exec out/fble/bin/fble-disassemble.cov -I spec -m $::mpath
     }
@@ -96,4 +115,5 @@ if { [catch dispatch msg] } {
   puts "$msg"
   exit 1
 }
+exit 0
 
