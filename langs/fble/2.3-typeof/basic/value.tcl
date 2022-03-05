5aacc6d39301094671cd86e29e8767062b5f2d45
Richard Uhler <ruhler@degralder.com>
2022-03-05 09:08:22 -0800

    Migrate 2.3-typeof tests to new format.

--- langs/fble/2.3-typeof/basic/value.tcl
+++
@@ -1,9 +0,0 @@
-fble-test {
-  @ Unit@ = *();
-  @ Bool@ = +(Unit@ true, Unit@ false);
-  Bool@ True = Bool@(true: Unit@());
-
-  # Take typeof a value.
-  @<True> False = Bool@(false: Unit@());
-  False.false;
-}
--- langs/fble/2.3-typeof/regression/func.tcl
+++
@@ -1,6 +0,0 @@
-fble-test {
-  # Regression test to verify we don't leak memory when doing typeof a
-  # function literal.
-  @ Unit@ = *();
-  @< (Unit@ x) { x; } >;
-}
