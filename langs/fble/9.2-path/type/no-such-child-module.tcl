d386f557f8e00cf184812c49e2e3033a3653b000
Richard Uhler <ruhler@degralder.com>
2022-03-07 08:17:15 -0800

    Port rest of 9.2-path tests to new format.

--- langs/fble/9.2-path/type/no-such-child-module.tcl
+++
@@ -1,6 +0,0 @@
-fble-test-compile-error 3:12 {
-  # The module /Unit/Foo% does not exist.
-  % Unit = /Unit/Foo%.Unit;
-
-  Unit;
-}
--- langs/fble/9.2-path/type/no-such-module.tcl
+++
@@ -1,6 +0,0 @@
-fble-test-compile-error 3:12 {
-  # The module /Unit% does not exist.
-  % Unit = /Unit%.Unit;
-
-  Unit;
-}
