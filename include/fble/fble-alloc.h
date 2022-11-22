a0f8e921a8eef3050b889d5fdee1a8bf723a7c86
Richard Uhler <ruhler@degralder.com>
2022-11-22 12:51:50 -0800

    Convert fble-alloc.h and fble-arg-parse.h to doxygen format.

--- include/fble/fble-alloc.h
+++ include/fble/fble-alloc.h
@@ -1,196 +1,197 @@
-// fble-alloc.h --
-//   Header file describing memory allocation functionality for fble.
+/**
+ * @file fble-alloc.h
+ * Header for memory allocation routines.
+ */
 
 #ifndef FBLE_ALLOC_H_
 #define FBLE_ALLOC_H_
 
 #include <sys/types.h>    // for size_t
 
-// FbleRawAlloc --
-//   Allocate size bytes of memory. This function is not type safe. It is
-//   recommended to use the FbleAlloc and FbleArrayAlloc macros instead.
-//
-// Inputs:
-//   size - The number of bytes to allocate.
-//
-// Result:
-//   A pointer to a newly allocated size bytes of memory.
-//
-// Side effects:
-// * Allocates size bytes that should be freed by calling FbleFree when no
-//   longer in use.
+/**
+ * Allocates 'size' bytes of memory. This function is not type safe. It is
+ * recommended to use the FbleAlloc and FbleArrayAlloc macros instead.
+ *
+ * @param size The number of bytes to allocate.
+ * @returns  A pointer to a newly allocated size bytes of memory.
+ * @sideeffects
+ * * Allocates size bytes that should be freed by calling FbleFree when no
+ *   longer in use.
+ */
 void* FbleRawAlloc(size_t size);
 
-// FbleAlloc --
-//   A type safe way of allocating objects.
-//
-// Inputs:
-//   T - The type of object to allocate.
-//
-// Results:
-//   A pointer to a newly allocated object of the given type.
-//
-// Side effects:
-// * The allocation should be freed by calling FbleFree when no longer in use.
+/**
+ * Type safe object allocation.
+ *
+ * @param T The type of object to allocate.
+ *
+ * @returns
+ *   A pointer to a newly allocated object of the given type.
+ *
+ * @sideeffects
+ * * The allocation should be freed by calling FbleFree when no longer in use.
+ */
 #define FbleAlloc(T) ((T*) FbleRawAlloc(sizeof(T)))
 
-// FbleAllocExtra --
-//   Allocate an object with additional extra space. For use with objects like:
-//   struct {
-//     ...
-//     Foo foo[]
-//   };
-//
-// Inputs:
-//   T - The type of object to allocate.
-//   size - The size of the extra space to include.
-//
-// Results:
-//   A pointer to a newly allocated object of the given type with extra size.
-//
-// Side effects:
-// * The allocation should be freed by calling FbleFree when no longer in use.
+/**
+ * Allocate object with extra space.
+ *
+ * For use with objects like:
+ *   struct {
+ *     ...
+ *     Foo foo[]
+ *   };
+ *
+ * @param T     The type of object to allocate.
+ * @param size  The size of the extra space to include.
+ *
+ * @returns
+ *   A pointer to a newly allocated object of the given type with extra size.
+ *
+ * @sideeffects
+ * * The allocation should be freed by calling FbleFree when no longer in use.
+ */
 #define FbleAllocExtra(T, size) ((T*) FbleRawAlloc(sizeof(T) + size))
 
-// FbleArrayAlloc --
-//   A type safe way of allocating an array of objects.
-//
-// Inputs:
-//   T - the type of object to allocate
-//   count - the number of objects in the array to allocate.
-//
-// Results:
-//   A pointer to a newly allocated array of count objects of the given type.
-//
-// Side effects:
-// * The allocation should be freed by calling FbleFree when no longer in use.
+/**
+ * Type safe array allocation.
+ *
+ * @param T       the type of object to allocate
+ * @param count   the number of objects in the array to allocate.
+ *
+ * @returns
+ *   A pointer to a newly allocated array of count objects of the given type.
+ *
+ * @sideeffects
+ * * The allocation should be freed by calling FbleFree when no longer in use.
+ */
 #define FbleArrayAlloc(T, count) ((T*) FbleRawAlloc((count) * sizeof(T)))
 
-// FbleFree --
-//   Free a memory allocation.
-//
-// Inputs:
-//   ptr - The allocation to free. May be NULL.
-//
-// Side effects:
-// * Frees memory associated with the ptr.
-// * After this call, any accesses to the freed memory result in undefined
-//   behavior.
-// * Behavior is undefined if ptr was not previously returned by a call to
-//   FbleAlloc or one of its variants.
+/**
+ * Free a memory allocation.
+ *
+ * @param ptr   The allocation to free. May be NULL.
+ *
+ * @sideeffects
+ * * Frees memory associated with the ptr.
+ * * After this call, any accesses to the freed memory result in undefined
+ *   behavior.
+ * * Behavior is undefined if ptr was not previously returned by a call to
+ *   FbleAlloc or one of its variants.
+ */
 void FbleFree(void* ptr);
 
-// FbleStackAllocator --
-//   Abstract type of an allocator to use for allocations.
+/** Abstract type of an allocator to use for allocations. */
 typedef struct FbleStackAllocator FbleStackAllocator;
 
-// FbleNewStackAllocator --
-//   Create a new stack allocator.
-//
-// Returns:
-//   A newly allocated stack allocator.
-//
-// Side effects:
-//   Allocates a stack allocator that should be freed using
-//   FbleFreeStackAllocator when no longer in use.
+/**
+ * Create a new stack allocator.
+ *
+ * @returns
+ *   A newly allocated stack allocator.
+ *
+ * @sideeffects
+ * * Allocates a stack allocator that should be freed using
+ *   FbleFreeStackAllocator when no longer in use.
+ */
 FbleStackAllocator* FbleNewStackAllocator();
 
-// FbleFreeStackAllocator --
-//   Free resources associated with a stack allocator.
-//
-// Inputs:
-//   allocator - the allocator to free.
-//
-// Side effects:
-// * Frees resources associated with the given allocator.
-// * The allocator must not have any outstanding allocations at the time of
-//   free.
+/**
+ * Free resources associated with a stack allocator.
+ *
+ * @param allocator the allocator to free.
+ *
+ * @sideeffects
+ * * Frees resources associated with the given allocator.
+ * * The allocator must not have any outstanding allocations at the time of
+ *   free.
+ */
 void FbleFreeStackAllocator(FbleStackAllocator* allocator);
 
-// FbleRawStackAlloc --
-//   Allocate a block of memory using the given stack allocator. This function
-//   is not type safe. It is recommended to use the FbleStackAlloc macros
-//   instead.
-//
-// Inputs:
-//   allocator - the allocator to allocate memory from.
-//   size - the number of bytes to allocate.
-//
-// Results:
-//   A block of memory allocated.
-//
-// Side effects:
-//   Allocates memory that should be freed with FbleStackFree when no longer
-//   needed.
+/**
+ * Allocate memory on stack.
+ *
+ * This function is not type safe. It is recommended to use the FbleStackAlloc
+ * macros instead.
+ *
+ * @param allocator   the allocator to allocate memory from.
+ * @param size        the number of bytes to allocate.
+ *
+ * @returns
+ *   A block of memory allocated.
+ *
+ * @sideeffects
+ * * Allocates memory that should be freed with FbleStackFree when no longer
+ *   needed.
+ */
 void* FbleRawStackAlloc(FbleStackAllocator* allocator, size_t size);
 
-// FbleStackAlloc --
-//   Type safe variant of FbleRawStackAlloc.
-//
-// Inputs:
-//   allocator - the allocator to allocate memory from.
-//   T - The type of object to allocate.
-//
-// Results:
-//   A pointer to a newly stack allocated object of the given type.
-//
-// Side effects:
-//   Allocates memory that should be freed with FbleStackFree when no longer
-//   needed.
+/**
+ * Type safe stack allocation.
+ *
+ * @param allocator   The allocator to allocate memory from.
+ * @param T           The type of object to allocate.
+ * 
+ * @returns
+ *   A pointer to a newly stack allocated object of the given type.
+ *
+ * @sideeffects
+ *   Allocates memory that should be freed with FbleStackFree when no longer
+ *   needed.
+ */
 #define FbleStackAlloc(allocator, T) ((T*) FbleRawStackAlloc(allocator, sizeof(T)))
 
-// FbleStackAllocExtra --
-//   Stack allocate an object with additional extra space. For use with
-//   objects like:
-//   struct {
-//     ...
-//     Foo foo[]
-//   };
-//
-// Inputs:
-//   allocator - The stack allocator.
-//   T - The type of object to allocate.
-//   size - The size of the extra space to include.
-//
-// Results:
-//   A pointer to a newly allocated object of the given type with extra size.
-//
-// Side effects:
-// * The allocation should be freed by calling FbleStackFree when no longer in
-//   use.
+/**
+ * Allocate stack object with extra space.
+ *
+ * For use with objects like:
+ *   struct {
+ *     ...
+ *     Foo foo[]
+ *   };
+ *
+ * @param allocator   The stack allocator.
+ * @param T           The type of object to allocate.
+ * @param size        The size of the extra space to include.
+ *
+ * @returns
+ *   A pointer to a newly allocated object of the given type with extra size.
+ *
+ * @sideeffects
+ * * The allocation should be freed by calling FbleStackFree when no longer in
+ *   use.
+ */
 #define FbleStackAllocExtra(allocator, T, size) ((T*) FbleRawStackAlloc(allocator, sizeof(T) + size))
 
-// FbleStackFree --
-//   Free a block of allocated memory.
-//
-// Inputs:
-//   allocator - the allocator to free the memory from.
-//   ptr - the block of memory to free.
-//
-// Side effects:
-// * Frees resources associated with the given pointer.
-// * Behavior is undefined if 'ptr' is not the same as the pointer most
-//   recently returned from FbleStackAlloc on this same allocator.
+/**
+ * Free a block of allocated memory.
+ *
+ * @param allocator   the allocator to free the memory from.
+ * @param ptr         the block of memory to free.
+ *
+ * @sideeffects
+ * * Frees resources associated with the given pointer.
+ * * Behavior is undefined if 'ptr' is not the same as the pointer most
+ *   recently returned from FbleStackAlloc on this same allocator.
+ */
 void FbleStackFree(FbleStackAllocator* allocator, void* ptr);
 
-// FbleMaxTotalBytesAllocated
-//   Returns the maximum number of bytes allocated using the fble allocation
-//   routines since the most recent call to FbleResetMaxTotalBytesAllocated.
-//
-// Results:
-//   The high water mark of allocations in total number of bytes allocated.
-//
-// Side effects:
-//   None.
+/**
+ * Get max bytes allocated.
+ *
+ * @returns
+ *   The high watermark of bytes allocated using the fble allocation routines
+ *   since the most recent call to FbleResetMaxTotalBytesAllocated.
+ */
 size_t FbleMaxTotalBytesAllocated();
 
-// FbleResetMaxTotalBytesAllocated
-//   Resets the max total alloc size to the current number of bytes
-//   allocated using the fble allocation routines.
-//
-// Side effects:
-// * Resets the max total alloc size to the current number of bytes allocated
-//   using the fble allocation routines.
+/**
+ * Reset max bytes allocated.
+ *
+ * @sideeffects
+ * * Resets the max total alloc size to the current number of bytes allocated
+ *   using the fble allocation routines.
+ */
 void FbleResetMaxTotalBytesAllocated();
 
 #endif // FBLE_ALLOC_H_
--- include/fble/fble-arg-parse.h
+++ include/fble/fble-arg-parse.h
@@ -1,5 +1,7 @@
-// fble-arg-parse.h --
-//   Header file for command line argument parsing library.
+/**
+ * @file fble-arg-parse.h
+ * Header for command line argument parsing library.
+ */
 
 #ifndef FBLE_ARG_PARSE_H_
 #define FBLE_ARG_PARSE_H_
@@ -8,97 +10,95 @@
 
 #include "fble-load.h"  // for FbleSearchPath
 
-// FbleParseBoolArg --
-//   Parse a boolean flag command line argument.
-//
-// Sample argument usage:
-//   --foo
-//
-// The flag may be specified at most once on the command line.
-//
-// Inputs:
-//   name - the name of the flag to parse, such as "--foo".
-//   dest - boolean value to update based on the flag setting.
-//   argc - pointer to number of arguments remaining.
-//   argv - pointer to arguments remaining.
-//   error - boolean value to set in case of error.
-//
-// Results:
-//   Returns true if the next argument matches the given name, false
-//   otherwise.
-//
-// Side effects:
-// * There will only be side effects if the function returns true.
-// * Updates dest with the value of the parsed argument.
-// * Advances argc and argv to the next argument.
-// * Sets error to true in case of error parsing the matched argument.
-// * Prints an error message to stderr in case of error.
+/**
+ * Parse a boolean flag command line argument.
+ *
+ * Sample argument usage:
+ *   --foo
+ *
+ * The flag may be specified at most once on the command line.
+ *
+ * @param name    the name of the flag to parse, such as "--foo".
+ * @param dest    boolean value to update based on the flag setting.
+ * @param argc    pointer to number of arguments remaining.
+ * @param argv    pointer to arguments remaining.
+ * @param error   boolean value to set in case of error.
+ *
+ * @returns
+ *   true if the next argument matches the given name, false otherwise.
+ *
+ * @sideeffects
+ * * There will only be side effects if the function returns true.
+ * * Updates dest with the value of the parsed argument.
+ * * Advances argc and argv to the next argument.
+ * * Sets error to true in case of error parsing the matched argument.
+ * * Prints an error message to stderr in case of error.
+ */
 bool FbleParseBoolArg(const char* name, bool* dest, int* argc, const char*** argv, bool* error);
 
-// FbleParseStringArg --
-//   Parse a string command line argument.
-//
-// Sample argument usage:
-//   --foo value
-// 
-// The flag may be specified at most once on the command line.
-//
-// Inputs:
-//   name - the name of the flag to parse, such as "--foo".
-//   dest - string value to update based on the flag setting.
-//   argc - pointer to number of arguments remaining.
-//   argv - pointer to arguments remaining.
-//   error - boolean value to set in case of error.
-//
-// Results:
-//   Returns true if the next argument matches the given name, false
-//   otherwise.
-//
-// Side effects:
-// * There will only be side effects if the function returns true.
-// * Updates dest with the value of the parsed argument.
-// * Advances argc and argv to the next argument.
-// * Sets error to true in case of error parsing the matched argument.
-// * Prints an error message to stderr in case of error.
+/**
+ * Parse a string command line argument.
+ *
+ * Sample argument usage:
+ *   --foo value
+ * 
+ * The flag may be specified at most once on the command line.
+ *
+ * @param name    the name of the flag to parse, such as "--foo".
+ * @param dest    string value to update based on the flag setting.
+ * @param argc    pointer to number of arguments remaining.
+ * @param argv    pointer to arguments remaining.
+ * @param error   boolean value to set in case of error.
+ *
+ * @returns
+ *   true if the next argument matches the given name, false otherwise.
+ *
+ * @sideeffects
+ * * There will only be side effects if the function returns true.
+ * * Updates dest with the value of the parsed argument.
+ * * Advances argc and argv to the next argument.
+ * * Sets error to true in case of error parsing the matched argument.
+ * * Prints an error message to stderr in case of error.
+ */
 bool FbleParseStringArg(const char* name, const char** dest, int* argc, const char*** argv, bool* error);
 
-// FbleParseSearchPathArg --
-//   Parse an FbleSearchPath command line argument.
-//
-// Sample argument usage:
-//   -I entry1 -Ientry2
-// 
-// Inputs:
-//   dest - search path value to append to based on the flag setting.
-//   argc - pointer to number of arguments remaining.
-//   argv - pointer to arguments remaining.
-//   error - boolean value to set in case of error.
-//
-// Results:
-//   Returns true if the next argument matches the given name, false
-//   otherwise.
-//
-// Side effects:
-// * There will only be side effects if the function returns true.
-// * Updates dest with the value of the parsed argument.
-// * Advances argc and argv to the next argument.
-// * Sets error to true in case of error parsing the matched argument.
-// * Prints an error message to stderr in case of error.
+/**
+ * Parse an FbleSearchPath command line argument.
+ *
+ * Sample argument usage:
+ *   -I entry1 -Ientry2
+ * 
+ * @param dest    search path value to append to based on the flag setting.
+ * @param argc    pointer to number of arguments remaining.
+ * @param argv    pointer to arguments remaining.
+ * @param error   boolean value to set in case of error.
+ *
+ * @returns
+ *   true if the next argument matches the given name, false otherwise.
+ *
+ * @sideeffects
+ * * There will only be side effects if the function returns true.
+ * * Updates dest with the value of the parsed argument.
+ * * Advances argc and argv to the next argument.
+ * * Sets error to true in case of error parsing the matched argument.
+ * * Prints an error message to stderr in case of error.
+ */
 bool FbleParseSearchPathArg(FbleSearchPath* dest, int* argc, const char*** argv, bool* error);
 
-// FbleParseInvalidArg --
-//   Pretend to parse an arg, but actually just indicate error.
-//
-// Inputs:
-//   argc - pointer to number of arguments remaining.
-//   argv - pointer to arguments remaining.
-//   error - boolean value to set in case of error.
-//
-// Results:
-//   Returns true.
-//
-// Side effects:
-// * Prints an error message to stderr and sets error to true.
+/**
+ * Indicate a command line error.
+ *
+ * Pretends to parse an arg, but actually just indicate error.
+ *
+ * @param argc    pointer to number of arguments remaining.
+ * @param argv    pointer to arguments remaining.
+ * @param error   boolean value to set in case of error.
+ *
+ * @returns true.
+ *
+ * @sideeffects
+ * * Prints an error message to stderr and sets error to true.
+ */
 bool FbleParseInvalidArg(int* argc, const char*** argv, bool* error);
 
 #endif // FBLE_ARG_PARSE_H_
