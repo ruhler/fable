21f6013d747a388145371cf418900686c770f6fb
Richard Uhler <ruhler@degralder.com>
2022-12-01 21:23:46 -0800

    Fix roff output for fbld.usage.tcl.
    By using troff commands instead of macros.

--- fbld.usage.tcl
+++ fbld.usage.tcl
@@ -3,8 +3,8 @@
 source fbld.tcl
 
 # Generate a man page.
-# @arg input - The name of a .fbld file describing the usage.
-# @output output - The name of the output man page file.
+# @title   Title to use for the man page.
+# @input   The name of a .fbld file describing the usage.
 proc man {title input} {
   # default command for line text.
   proc inline_ {text} {
@@ -125,4 +125,117 @@ proc man {title input} {
   ::fbld::block block_invoke [read [open $input "r"]]
 }
 
-man [lindex $argv 0] [lindex $argv 1]
+# Generate roff formatted usage text.
+# @arg input - The name of a .fbld file describing the usage.
+proc roff {input} {
+  # default command for line text.
+  proc inline_ {text} {
+    puts -nonewline [::fbld::unescape $text]
+  }
+
+  proc inline_a {text} { inline_ $text }
+  proc inline_l {text} { inline_ $text }
+
+  proc inline_invoke {cmd args} {
+    inline_$cmd {*}$args
+  }
+
+  # @name[text][text]
+  # The name and brief description of the command.
+  proc block_name {name brief} {
+    puts "$name \\- [string trim $brief]"
+    puts ".br"
+  }
+
+  # @usage[inline]
+  # Renders the usage text used in a synopsis.
+  proc block_usage {text} {
+    puts -nonewline "Usage: "
+    ::fbld::inline inline_invoke [string trim $text]
+    puts "\n.br"
+  }
+
+  # @description[block]
+  proc block_description {text} {
+    ::fbld::block block_invoke $text
+  }
+
+  # @par[inline]
+  # Default paragraph block.
+  proc block_par {text} {
+    puts ".sp"
+    ::fbld::inline inline_invoke [string trim $text]
+    puts "\n.br"
+  }
+
+  # @defs[block]
+  # Used for a definition list.
+  proc block_defs {text} {
+    ::fbld::block block_invoke $text
+  }
+
+  # @definition[name][value]
+  # Used for a long definition.
+  proc block_definition {name text} {
+    puts ".sp"
+    ::fbld::inline inline_invoke [string trim $name]
+    puts ":\n.br\n.in +2"
+    ::fbld::block block_invoke $text
+    puts "\n.in -2"
+  }
+
+  # @def[name][value]
+  # Used for a short definition.
+  proc block_def {name text} {
+    ::fbld::inline inline_invoke [string trim $name]
+    puts "\n.br\n.in +4"
+    ::fbld::inline inline_invoke [string trim $text]
+    puts "\n.br\n.in -4"
+  }
+
+  proc block_options {text} {
+    ::fbld::block block_invoke $text
+  }
+
+  proc block_exitstatus {text} {
+    puts "Exit Status:\n.br\n.in +2"
+    ::fbld::block block_invoke $text
+    puts "\n.in -2"
+  }
+
+  proc block_subsection {title body} {
+    puts "$title:\n.br\n.in +2"
+    ::fbld::block block_invoke $body
+    puts "\n.in -2"
+  }
+
+  proc block_opt {opt desc} {
+    block_def $opt $desc
+  }
+
+  proc block_examples {text} {
+    puts "Examples:\n.br\n.in +2"
+    ::fbld::block block_invoke $text
+    puts "\n.in -2"
+  }
+
+  proc block_ex {text desc} {
+    puts ".sp"
+    ::fbld::inline inline_invoke [string trim $text]
+    puts "\n.sp"
+    ::fbld::block block_invoke $desc
+  }
+
+  proc block_ {text} {
+    block_par $text
+  }
+
+  proc block_invoke {cmd args} {
+    block_$cmd {*}$args
+  }
+
+  ::fbld::block block_invoke [read [open $input "r"]]
+}
+
+#man [lindex $argv 0] [lindex $argv 1]
+roff [lindex $argv 0]
