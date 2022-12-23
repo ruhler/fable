3ed95952a0c458a2684f0f202dc1aa322fa9577a
Richard Uhler <ruhler@degralder.com>
2022-12-23 11:49:40 -0800

    Add documentation section to readme.
    With links to spec, tutorials, C API, and internals. It includes source
    links for people reading the README.adoc file directly, and generated
    links for people viewing the generated $BUILD/www/index.html file.
    
    Not all of the links are set up to work yet.

--- www/build.tcl
+++ www/build.tcl
@@ -1,6 +1,8 @@
 namespace eval "www" {
   build "$::b/www/index.html" "$::s/README.adoc" \
     "asciidoc -o $::b/www/index.html $::s/README.adoc"
+  build "$::b/www/spec/fble.html" "$::s/spec/fble.adoc" \
+    "asciidoc -o $::b/www/spec/fble.html $::s/spec/fble.adoc"
 
-  phony "www" [list $::b/www/index.html]
+  phony "www" [list $::b/www/index.html $::b/www/spec/fble.html doxygen]
 }
