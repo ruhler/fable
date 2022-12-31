56ed01cb02bfb83ad6525e373acdf412d07d5dec
Richard Uhler <ruhler@degralder.com>
2022-12-31 13:24:05 -0800

    Rename fble.test.tcl test.tcl.

--- fbld/build.tcl
+++ fbld/build.tcl
@@ -1,4 +1,4 @@
 namespace eval "fbld" {
-  test $::b/fbld/fbld.test.tr $::s/fbld/fbld.test.tcl \
-    "tclsh8.6 $::s/fbld/fbld.test.tcl"
+  test $::b/fbld/test.tr $::s/fbld/test.tcl \
+    "tclsh8.6 $::s/fbld/test.tcl"
 }
