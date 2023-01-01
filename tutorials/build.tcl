4c393bc909e031bc379b3efc20031ca3b97c8d2c
Richard Uhler <ruhler@degralder.com>
2023-01-01 10:50:07 -0800

    Draft code for Modules tutorial.

--- tutorials/build.tcl
+++ tutorials/build.tcl
@@ -15,4 +15,15 @@ namespace eval "tutorials" {
   test $::b/tutorials/Basics.tr \
     "$::b/pkgs/core/fble-stdio $::s/tutorials/Basics/Basics.fble" \
     "$::b/pkgs/core/fble-stdio -I $::s/pkgs/core -I $::s/tutorials/Basics -m /Basics%"
+
+  # Modules tests
+  test $::b/tutorials/Modules.tr [list \
+    $::b/pkgs/core/fble-stdio \
+    $::s/tutorials/Modules/Unit.fble \
+    $::s/tutorials/Modules/Bit.fble \
+    $::s/tutorials/Modules/Bit/Show.fble \
+    $::s/tutorials/Modules/Bit4.fble \
+    $::s/tutorials/Modules/Bit4/Show.fble \
+    $::s/tutorials/Modules/Main.fble] \
+    "$::b/pkgs/core/fble-stdio -I $::s/pkgs/core -I $::s/tutorials/Modules -m /Main%"
 }
