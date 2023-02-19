9a94397d2a871a8bef1c448ce366f00e1d666501
Richard Uhler <ruhler@degralder.com>
2023-02-19 09:48:45 -0800

    Add dist targets to spec/build.tcl.

--- spec/build.tcl
+++ spec/build.tcl
@@ -1,5 +1,19 @@
 namespace eval "spec" {
+  dist_s $::s/spec/build.tcl
+  dist_s $::s/spec/fble.fbld
+  dist_s $::s/spec/fble.fbld.tcl
+  dist_s $::s/spec/fble.lang
+  dist_s $::s/spec/fble.style.txt
+  dist_s $::s/spec/README.fbld
+
+  foreach dir [dirs $::s/spec/SpecTests ""] {
+    foreach {x} [build_glob $::s/spec/SpecTests/$dir -nocomplain -type f *] {
+      dist_s $x
+    }
+  }
+
   # Fbld spec www.
-  ::html_doc $::b/www/spec/fble.html $::s/spec/fble.fbld $::s/spec/fble.fbld.tcl
-  www $::b/www/spec/fble.html
+  ::html_doc $::d/www/spec/fble.html $::s/spec/fble.fbld $::s/spec/fble.fbld.tcl
+  www $::d/www/spec/fble.html
+  dist_d $::d/www/spec/fble.html
 }
