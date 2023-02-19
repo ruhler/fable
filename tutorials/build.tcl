13003c2b8f079f1d0034cad126414c8d10a4bc8d
Richard Uhler <ruhler@degralder.com>
2023-02-19 09:42:18 -0800

    Add dist targets to tutorials/build.tcl.

--- tutorials/build.tcl
+++ tutorials/build.tcl
@@ -1,4 +1,54 @@
 namespace eval "tutorials" {
+  set tutorials {
+    AbstractTypes.fbld
+    Basics.fbld
+    Bind.fbld
+    Core.fbld
+    Functions.fbld
+    HelloWorld.fbld
+    HelloWorldRevisited.fbld
+    Install.fbld
+    Introduction.fbld
+    Lists.fbld
+    Literals.fbld
+    Modules.fbld
+    Polymorphism.fbld
+    Structs.fbld
+    Unions.fbld
+    Variables.fbld
+  }
+
+  set modules_tutorial_files {
+    Unit.fble
+    Bit.fble
+    Bit/Show.fble
+    Bit4.fble
+    Bit4/Show.fble
+    Main.fble
+  }
+
+  dist_s $::s/tutorials/build.tcl
+  dist_s $::s/tutorials/Tutorials.fbld
+  dist_s $::s/tutorials/debug_guide.txt
+  dist_s $::s/tutorials/profile_guide.txt
+  dist_s $::s/tutorials/Basics/Basics.fble
+  dist_s $::s/tutorials/HelloWorld/HelloWorld.fble
+  foreach {x} $tutorials { dist_s $::s/tutorials/$x }
+  foreach {x} $modules_tutorial_files { dist_s $::s/tutorials/Modules/$x }
+
+  # Tutorial table of contents
+  ::html_doc $::d/www/tutorials/Tutorials.html $::s/tutorials/Tutorials.fbld
+  www $::d/www/tutorials/Tutorials.html
+  dist_d $::d/www/tutorials/Tutorials.html
+
+  # Tutorials
+  foreach {x} $tutorials {
+    set base [file rootname [file tail $x]]
+    ::html_tutorial $::d/www/tutorials/$base.html $::s/tutorials/$base.fbld
+    www $::d/www/tutorials/$base.html
+    dist_d $::d/www/tutorials/$base.html
+  }
+
   # HelloWorld tests
   test $::b/tutorials/HelloWorld.tr \
     "$::b/pkgs/core/fble-stdio $::s/tutorials/HelloWorld/HelloWorld.fble" \
@@ -10,28 +60,10 @@ namespace eval "tutorials" {
     "$::b/pkgs/core/fble-stdio -I $::s/pkgs/core -I $::s/tutorials/Basics -m /Basics%"
 
   # Modules tests
-  test $::b/tutorials/Modules.tr [list \
-    $::b/pkgs/core/fble-stdio \
-    $::s/tutorials/Modules/Unit.fble \
-    $::s/tutorials/Modules/Bit.fble \
-    $::s/tutorials/Modules/Bit/Show.fble \
-    $::s/tutorials/Modules/Bit4.fble \
-    $::s/tutorials/Modules/Bit4/Show.fble \
-    $::s/tutorials/Modules/Main.fble] \
-    "$::b/pkgs/core/fble-stdio -I $::s/pkgs/core -I $::s/tutorials/Modules -m /Main%"
-
-  # Tutorial table of contents
-  ::html_doc $::b/www/tutorials/Tutorials.html $::s/tutorials/Tutorials.fbld
-  www $::b/www/tutorials/Tutorials.html
-
-  # Tutorials
-  foreach {x} [build_glob $::s/tutorials *.fbld] {
-    if [string equal $x $::s/tutorials/Tutorials.fbld] {
-      continue
-    }
-
-    set base [file rootname [file tail $x]]
-    ::html_tutorial $::b/www/tutorials/$base.html $::s/tutorials/$base.fbld
-    www $::b/www/tutorials/$base.html
+  set modules_deps [list $::b/pkgs/core/fble-stdio]
+  foreach {x} $modules_tutorial_files {
+    lappend modules_deps $::s/tutorials/Modules/$x
   }
+  test $::b/tutorials/Modules.tr $modules_deps \
+    "$::b/pkgs/core/fble-stdio -I $::s/pkgs/core -I $::s/tutorials/Modules -m /Main%"
 }
