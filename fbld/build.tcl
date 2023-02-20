d58e045e9568b4de5d43691572d05bb78968b85f
Richard Uhler <ruhler@degralder.com>
2023-02-20 11:59:18 -0800

    Fix missing fbld dependency on fbld.lang and fbld.lang.

--- fbld/build.tcl
+++ fbld/build.tcl
@@ -12,8 +12,9 @@ namespace eval "fbld" {
   #
   #   target - the file to generate
   #   source - the .fbld file
+  #   deps - additional dependencies
   #   args - list of fbld processing scripts to use.
-  proc ::fbld { target source args } {
+  proc ::fbld { target source deps args } {
     build $target \
       "$source $::s/fbld/fbld.tcl $::s/fbld/runfbld.tcl $args" \
       "tclsh8.6 $::s/fbld/fbld.tcl $::s/fbld/runfbld.tcl $args < $source > $target"
@@ -23,7 +24,9 @@ namespace eval "fbld" {
   #
   # args is additional optional fbld processing scripts.
   proc ::html_doc { target source args } {
-    fbld $target $source [list $::s/fbld/html.tcl {*}$args]
+    fbld $target $source \
+      "$::s/fbld/fbld-lang $::s/spec/fble.lang" \
+      [list $::s/fbld/html.tcl {*}$args]
   }
 
   # Builds an html file from an fbld @tutorial.
