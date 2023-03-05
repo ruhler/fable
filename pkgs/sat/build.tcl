16af2f9d0dc614ea14c009d6c081e5f306b4b499
Richard Uhler <ruhler@degralder.com>
2023-03-05 09:35:13 -0800

    Add man page for fble-sat.

--- pkgs/sat/build.tcl
+++ pkgs/sat/build.tcl
@@ -2,6 +2,7 @@ namespace eval "pkgs/sat" {
   dist_s $::s/pkgs/sat/bench/README.txt
   dist_s $::s/pkgs/sat/bench/unif-c1000-v500-s1356655268.cnf
   dist_s $::s/pkgs/sat/build.tcl
+  dist_s $::s/pkgs/sat/sat.fbld
   dist_s $::s/pkgs/sat/thoughts.txt
   dist_s $::s/pkgs/sat/TODO.txt
 
@@ -21,6 +22,8 @@ namespace eval "pkgs/sat" {
   stdio $::b/pkgs/sat/sat-bench "/Sat/Bench%" "sat"
 
   # fble-sat binary
+  man_usage $::b/pkgs/sat/fble-sat.1 $::s/pkgs/sat/sat.fbld
+  install $::b/pkgs/sat/fble-sat.1 $::config::mandir/man1/fble-sat.1
   stdio $::b/pkgs/sat/fble-sat "/Sat/Main%" "sat"
   install $::b/pkgs/sat/fble-sat $::config::bindir/fble-sat
 }
