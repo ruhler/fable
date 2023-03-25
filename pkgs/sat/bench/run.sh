6fd03603c7e110aaad1e2b3d850c0eaf5d6a0f70
Richard Uhler <ruhler@degralder.com>
2023-03-25 09:55:52 -0700

    Remove pkgs/sat/bench directory.
    It's not scalable to check in a bunch of sat benchmarks. Anyone who
    wants to run those benchmarks can download them and run them on their
    own.

--- pkgs/sat/bench/run.sh
+++
@@ -1,7 +0,0 @@
-# Usage: bash run.sh fble-sat DIR
-# Run fble-sat on all .cnf files in the given directory.
-
-for f in `find $2 -type f -name '*.cnf'`; do
-  echo $f
-  timeout 5m /usr/bin/time -f %E $1 < $f 
-done
---
+++ pkgs/sat/run-bench.sh
@@ -0,0 +1,13 @@
+# Usage: bash run-bench.sh fble-sat DIR
+# Run fble-sat on all .cnf files in the given directory.
+#
+# For example, you could get benchmarks from:
+#   https://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/New/Competition-02/sat-2002-beta.tgz
+#
+# An easy problem to start with:
+#   sat-2002-beta/generated/gen-1/gen-1.1/unif-c1000-v500-s1356655268.cnf
+
+for f in `find $2 -type f -name '*.cnf'`; do
+  echo $f
+  timeout 2m /usr/bin/time -f %E $1 < $f 
+done
