753216e5a585dfdeffb7e36e7569e45006e421af
Richard Uhler <ruhler@degralder.com>
2023-05-24 07:04:26 -0700

    Add --fble frontend to fbld.
    For @AbstractSyntax, @ConcreteSyntax, and @Example used in the fble
    spec.

--- pkgs/fbld/build.tcl
+++ pkgs/fbld/build.tcl
@@ -18,14 +18,15 @@ namespace eval "pkgs/fbld" {
   stdio $::b/pkgs/fbld/fble-fbld "/Fbld/Main%" "fbld"
   install $::b/pkgs/fbld/fble-fbld $::config::bindir/fble-fbld
 
-  proc fbld_html_doc { target source } {
+  # args is additional command line options.
+  proc fbld_html_doc { target source args } {
     build $target \
       "$::b/pkgs/fbld/fble-fbld $source $::s/buildstamp" \
-      "$::b/pkgs/fbld/fble-fbld $source --html --version $::version --buildstamp `$::s/buildstamp` > $target"
+      "$::b/pkgs/fbld/fble-fbld $source --html --version $::version --buildstamp `$::s/buildstamp` $args > $target"
   }
 
   fbld_html_doc $::b/pkgs/fbld/fbld.html $::s/fbld/fbld.fbld
-  fbld_html_doc $::b/pkgs/fbld/fble.html $::s/spec/fble.fbld
+  fbld_html_doc $::b/pkgs/fbld/fble.html $::s/spec/fble.fbld --fble
   fbld_html_doc $::b/pkgs/fbld/README.html $::s/README.fbld
 
   # README.md
--- spec/fble.fbld.tcl
+++ spec/fble.fbld.tcl
@@ -1,20 +1,20 @@
 # Extra fbld tags used in fble.fbld
 
-# @AbstractSyntax[INLINE content]
+# @AbstractSyntax[ESCAPED content]
 # Describes the abstract syntax for a language construct.
 # @param content  The abstract syntax.
 proc block_AbstractSyntax {content} {
   ::block_definition "Abstract Syntax" "@code\[txt\]\[$content\]"
 }
 
-# @ConcreteSyntax[INLINE content]
+# @ConcreteSyntax[ESCAPED content]
 # Describes the concrete syntax for a language construct.
 # @param content  The concrete syntax.
 proc block_ConcreteSyntax {content} {
   ::block_definition "Concrete Syntax" "@code\[txt\]\[$content\]"
 }
 
-# @Example[INLINE content]
+# @Example[ESCAPED content]
 # Gives example fble code for a language construct.
 # @param content  The example code.
 proc block_Example {content} {
