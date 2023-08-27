64b71b2f07f421ca16576cdda9f96cc14f6a8386
Richard Uhler <ruhler@degralder.com>
2023-08-27 10:48:39 -0700

    Add fbld2 main program.
    All it does for now is parse.
    
    Also add a sketch of the fbld2 syntax reference.

--- pkgs/fbld2/build.tcl
+++ pkgs/fbld2/build.tcl
@@ -5,4 +5,8 @@ namespace eval "pkgs/fbld2" {
   stdio $::b/pkgs/fbld2/fbld-tests "/Fbld/Tests%" "fbld2"
   testsuite $::b/pkgs/fbld2/fbld-tests.tr $::b/pkgs/fbld2/fbld-tests \
     "$::b/pkgs/fbld2/fbld-tests"
+
+  # fbld2
+  stdio $::b/pkgs/fbld2/fbld2 "/Fbld/Main%" "fbld2"
+  install $::b/pkgs/fbld2/fbld2 $::config::bindir/fbld2
 }
