a7d2a7e1e9c99a49f34c8674b26b6d89b83f35fb
Richard Uhler <ruhler@degralder.com>
2023-09-08 07:22:20 -0700

    Add definitions of @FbleVersion and @Buildstamp for fbld2.

--- pkgs/fbld2/build.tcl
+++ pkgs/fbld2/build.tcl
@@ -9,4 +9,8 @@ namespace eval "pkgs/fbld2" {
   # fbld2
   stdio $::b/pkgs/fbld2/fbld2 "/Fbld/Main%" "fbld2"
   install $::b/pkgs/fbld2/fbld2 $::config::bindir/fbld2
+
+  # version.fbld
+  build $::b/fbld2/version.fbld "" \
+    "echo @define\[FbleVersion\]\[\]\[$::version\] @@ > $::b/fbld2/version.fbld"
 }
