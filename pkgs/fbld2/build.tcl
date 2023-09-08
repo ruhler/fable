b627390c1c35e0c5a6f0d9462d420493360b3d04
Richard Uhler <ruhler@degralder.com>
2023-09-08 07:32:15 -0700

    Add build rules for fbld2 based README.md and README.html

--- pkgs/fbld2/build.tcl
+++ pkgs/fbld2/build.tcl
@@ -13,4 +13,19 @@ namespace eval "pkgs/fbld2" {
   # version.fbld
   build $::b/fbld2/version.fbld "" \
     "echo @define\[FbleVersion\]\[\]\[$::version\] @@ > $::b/fbld2/version.fbld"
+
+  # README.md
+  build $::b/pkgs/fbld2/README.md \
+    "$::b/pkgs/fbld2/fbld2 $::s/fbld2/nobuildstamp.fbld $::b/fbld2/version.fbld $::s/fbld2/markdown.fbld $::s/README.2.fbld" \
+    "$::b/pkgs/fbld2/fbld2 $::s/fbld2/nobuildstamp.fbld $::b/fbld2/version.fbld $::s/fbld2/markdown.fbld $::s/README.2.fbld > $::b/pkgs/fbld2/README.md"
+
+  # Make sure the version of README.md checked in matches the latest version.
+  test $::b/pkgs/fbld2/README.tr \
+    "$::s/README.md $::b/pkgs/fbld2/README.md" \
+    "cmp $::s/README.md $::b/pkgs/fbld2/README.md"
+
+  # README.html
+  build $::b/pkgs/fbld2/README.html \
+    "$::b/pkgs/fbld2/fbld2 $::s/buildstamp $::b/fbld2/version.fbld $::s/fbld2/html.fbld $::s/README.2.fbld" \
+    "$::s/buildstamp --fbld2 BuildStamp | $::b/pkgs/fbld2/fbld2 - $::b/fbld2/version.fbld $::s/fbld2/html.fbld $::s/README.2.fbld > $::b/pkgs/fbld2/README.html"
 }
