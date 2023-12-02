6ed055a2fd5ef0f7b827d5e2bb98975b6b92bb39
Richard Uhler <ruhler@degralder.com>
2023-12-02 10:20:18 -0800

    Review and clean up pkgs/graphics fble code.
    Also, add back graphics tests to the test suite. For some reason it
    wasn't there.

--- pkgs/graphics/build.tcl
+++ pkgs/graphics/build.tcl
@@ -1,6 +1,11 @@
 namespace eval "pkgs/graphics" {
   pkg graphics [list core app] ""
 
+  # /Graphics/Tests% interpreted
+  set cflags "-I $::s/pkgs/graphics -I $::s/pkgs/app -I $::s/pkgs/core"
+  testsuite $::b/pkgs/graphics/tests-interpreted.tr "$::b/pkgs/core/fble-stdio $::b/pkgs/graphics/Graphics/Tests.fble.d" \
+    "$::b/pkgs/core/fble-stdio $cflags -m /Graphics/Tests%"
+
   if $::config::enable_fble_app {
     fbld_man_usage $::b/pkgs/graphics/fble-graphics.1 $::s/pkgs/graphics/fble-graphics.fbld
     install $::b/pkgs/graphics/fble-graphics.1 $::config::mandir/man1/fble-graphics.1
