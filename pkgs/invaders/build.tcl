73358ce6950f51480d88492ef85f92d83b3ee9cf
Richard Uhler <ruhler@degralder.com>
2023-12-02 11:14:36 -0800

    Review and update invaders library code.

--- pkgs/invaders/build.tcl
+++ pkgs/invaders/build.tcl
@@ -3,12 +3,12 @@ namespace eval "pkgs/invaders" {
 
   # /Invaders/Tests% interpreted
   set cflags "-I $::s/pkgs/invaders -I $::s/pkgs/app -I $::s/pkgs/core"
-  test $::b/pkgs/invaders/tests-interpreted.tr "$::b/pkgs/core/fble-stdio $::b/pkgs/invaders/Invaders/Tests.fble.d" \
+  testsuite $::b/pkgs/invaders/tests-interpreted.tr "$::b/pkgs/core/fble-stdio $::b/pkgs/invaders/Invaders/Tests.fble.d" \
     "$::b/pkgs/core/fble-stdio $cflags -m /Invaders/Tests%"
 
   # /Invaders/Tests% compiled
   stdio $::b/pkgs/invaders/invaders-tests "/Invaders/Tests%" "invaders app"
-  test $::b/pkgs/invaders/tests-compiled.tr $::b/pkgs/invaders/invaders-tests \
+  testsuite $::b/pkgs/invaders/tests-compiled.tr $::b/pkgs/invaders/invaders-tests \
     "$::b/pkgs/invaders/invaders-tests"
 
   if $::config::enable_fble_app {
