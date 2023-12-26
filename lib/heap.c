1db8acdd7feba39e241c8f1afae160a2955a5987
Richard Uhler <ruhler@degralder.com>
2023-12-26 10:51:50 -0800

    Distinguish between Obj* and ObjList*.
    Saves a couple seconds on the 2 minute runtime of generating fble.html.

--- lib/heap.c
+++ lib/heap.c
@@ -47,11 +47,18 @@ typedef enum {
 } Space;
 
 /**
+ * A list of objects.
+ */
+typedef struct ObjList {
+  struct ObjList* prev;   /**< Previous entry in doubly linked list of objects. */
+  struct ObjList* next;   /**< Next entry in doubly linked list of objects. */
+} ObjList;
+
+/**
  * An object allocated on the heap.
  */
 typedef struct Obj {
-  struct Obj* prev;   /**< Previous entry in doubly linked list of objects. */
-  struct Obj* next;   /**< Next entry in doubly linked list of objects. */
+  ObjList list;       /**< List of objects this object belongs to. */
   Space space;        /**< Which space the object currently belongs to. */
 
   /**
@@ -96,12 +103,12 @@ struct FbleHeap {
   /** The on_free callback to indicate an object has been freed. */
   void (*on_free)(FbleHeap* heap, void* obj);
 
-  Obj* to;          /**< List of non-root objects in the "to" space. */
-  Obj* from;        /**< List of non-root objects in the "from" space. */
-  Obj* pending;     /**< List of non-root objects in the PENDING space. */
-  Obj* roots_to;    /**< List of root objects in the "to" space. */
-  Obj* roots_from;  /**< List of root objects in the "from" and PENDING space. */
-  Obj* free;        /**< List of free objects. */
+  ObjList to;          /**< List of non-root objects in the "to" space. */
+  ObjList from;        /**< List of non-root objects in the "from" space. */
+  ObjList pending;     /**< List of non-root objects in the PENDING space. */
+  ObjList roots_to;    /**< List of root objects in the "to" space. */
+  ObjList roots_from;  /**< List of root objects in the "from" and PENDING space. */
+  ObjList free;        /**< List of free objects. */
 
   Space to_space;     /**< Which space, A or B, is currently the "to" space. */
   Space from_space;   /**< Which space, A or B, is currently the "from" space. */
@@ -129,10 +136,10 @@ static bool IncrGc(FbleHeap* heap)
   // object here, we won't be able to get ahead of allocations; the heap will
   // never shrink. We can shrink the heap if we free just a little more than
   // one object here. Two seems like the easiest approximation to that.
-  for (size_t i = 0; i < 2 && heap->free->next != heap->free; ++i) {
-    Obj* obj = heap->free->next;
-    obj->prev->next = obj->next;
-    obj->next->prev = obj->prev;
+  for (size_t i = 0; i < 2 && heap->free.next != &heap->free; ++i) {
+    Obj* obj = (Obj*)heap->free.next;
+    obj->list.prev->next = obj->list.next;
+    obj->list.next->prev = obj->list.prev;
     heap->on_free(heap, obj->obj);
     FbleFree(obj);
   }
@@ -148,61 +155,61 @@ static bool IncrGc(FbleHeap* heap)
   // Here we traverse exactly one object. We give priority to pending objects
   // in the hopes that roots will be dropped and can be collected this GC
   // cycle if we haven't traversed them yet.
-  if (heap->pending->next != heap->pending) {
-    Obj* obj = heap->pending->next;
-    obj->prev->next = obj->next;
-    obj->next->prev = obj->prev;
-    obj->next = heap->to->next;
-    obj->prev = heap->to;
+  if (heap->pending.next != &heap->pending) {
+    Obj* obj = (Obj*)heap->pending.next;
+    obj->list.prev->next = obj->list.next;
+    obj->list.next->prev = obj->list.prev;
+    obj->list.next = heap->to.next;
+    obj->list.prev = &heap->to;
     obj->space = heap->to_space;
-    heap->to->next->prev = obj;
-    heap->to->next = obj;
+    heap->to.next->prev = &obj->list;
+    heap->to.next = &obj->list;
     heap->refs(heap, obj->obj);
-  } else if (heap->roots_from->next != heap->roots_from) {
-    Obj* obj = heap->roots_from->next;
-    obj->prev->next = obj->next;
-    obj->next->prev = obj->prev;
-    obj->next = heap->roots_to->next;
-    obj->prev = heap->roots_to;
+  } else if (heap->roots_from.next != &heap->roots_from) {
+    Obj* obj = (Obj*)heap->roots_from.next;
+    obj->list.prev->next = obj->list.next;
+    obj->list.next->prev = obj->list.prev;
+    obj->list.next = heap->roots_to.next;
+    obj->list.prev = &heap->roots_to;
     obj->space = heap->to_space;
-    heap->roots_to->next->prev = obj;
-    heap->roots_to->next = obj;
+    heap->roots_to.next->prev = &obj->list;
+    heap->roots_to.next = &obj->list;
     heap->refs(heap, obj->obj);
   }
 
   // If we are done with gc, start a new GC by swapping from and to spaces.
-  if (heap->roots_from->next == heap->roots_from
-      && heap->pending->next == heap->pending) {
+  if (heap->roots_from.next == &heap->roots_from
+      && heap->pending.next == &heap->pending) {
     // Anything still in the "from" space is unreachable. Move it to the free
     // space.
-    if (heap->from->next != heap->from) {
-      heap->free->next->prev = heap->from->prev;
-      heap->from->prev->next = heap->free->next;
-      heap->free->next = heap->from->next;
-      heap->free->next->prev = heap->free;
+    if (heap->from.next != &heap->from) {
+      heap->free.next->prev = heap->from.prev;
+      heap->from.prev->next = heap->free.next;
+      heap->free.next = heap->from.next;
+      heap->free.next->prev = &heap->free;
     }
-    heap->from->next = heap->from;
-    heap->from->prev = heap->from;
+    heap->from.next = &heap->from;
+    heap->from.prev = &heap->from;
 
     // Move the "to" space to the "from" space.
-    if (heap->to->next != heap->to) {
-      heap->to->next->prev = heap->from;
-      heap->to->prev->next = heap->from;
-      heap->from->next = heap->to->next;
-      heap->from->prev = heap->to->prev;
+    if (heap->to.next != &heap->to) {
+      heap->to.next->prev = &heap->from;
+      heap->to.prev->next = &heap->from;
+      heap->from.next = heap->to.next;
+      heap->from.prev = heap->to.prev;
     }
-    heap->to->next = heap->to;
-    heap->to->prev = heap->to;
+    heap->to.next = &heap->to;
+    heap->to.prev = &heap->to;
 
     // Move the roots to the roots_from space.
-    if (heap->roots_to->next != heap->roots_to) {
-      heap->roots_to->next->prev = heap->roots_from;
-      heap->roots_to->prev->next = heap->roots_from;
-      heap->roots_from->next = heap->roots_to->next;
-      heap->roots_from->prev = heap->roots_to->prev;
+    if (heap->roots_to.next != &heap->roots_to) {
+      heap->roots_to.next->prev = &heap->roots_from;
+      heap->roots_to.prev->next = &heap->roots_from;
+      heap->roots_from.next = heap->roots_to.next;
+      heap->roots_from.prev = heap->roots_to.prev;
     }
-    heap->roots_to->next = heap->roots_to;
-    heap->roots_to->prev = heap->roots_to;
+    heap->roots_to.next = &heap->roots_to;
+    heap->roots_to.prev = &heap->roots_to;
 
     Space tmp = heap->to_space;
     heap->to_space = heap->from_space;
@@ -225,25 +232,18 @@ FbleHeap* FbleNewHeap(
   heap->refs = refs;
   heap->on_free = on_free;
 
-  heap->to = FbleAlloc(Obj);
-  heap->from = FbleAlloc(Obj);
-  heap->pending = FbleAlloc(Obj);
-  heap->roots_to = FbleAlloc(Obj);
-  heap->roots_from = FbleAlloc(Obj);
-  heap->free = FbleAlloc(Obj);
-
-  heap->to->prev = heap->to;
-  heap->to->next = heap->to;
-  heap->from->prev = heap->from;
-  heap->from->next = heap->from;
-  heap->pending->prev = heap->pending;
-  heap->pending->next = heap->pending;
-  heap->roots_to->prev = heap->roots_to;
-  heap->roots_to->next = heap->roots_to;
-  heap->roots_from->prev = heap->roots_from;
-  heap->roots_from->next = heap->roots_from;
-  heap->free->prev = heap->free;
-  heap->free->next = heap->free;
+  heap->to.prev = &heap->to;
+  heap->to.next = &heap->to;
+  heap->from.prev = &heap->from;
+  heap->from.next = &heap->from;
+  heap->pending.prev = &heap->pending;
+  heap->pending.next = &heap->pending;
+  heap->roots_to.prev = &heap->roots_to;
+  heap->roots_to.next = &heap->roots_to;
+  heap->roots_from.prev = &heap->roots_from;
+  heap->roots_from.next = &heap->roots_from;
+  heap->free.prev = &heap->free;
+  heap->free.next = &heap->free;
   heap->to_space = A;
   heap->from_space = B;
   return heap;
@@ -253,13 +253,6 @@ FbleHeap* FbleNewHeap(
 void FbleFreeHeap(FbleHeap* heap)
 {
   FbleHeapFullGc(heap);
-
-  FbleFree(heap->to);
-  FbleFree(heap->from);
-  FbleFree(heap->pending);
-  FbleFree(heap->roots_to);
-  FbleFree(heap->roots_from);
-  FbleFree(heap->free);
   FbleFree(heap);
 }
 
@@ -270,12 +263,12 @@ void* FbleNewHeapObject(FbleHeap* heap, size_t size)
 
   // Objects are allocated as roots.
   Obj* obj = FbleAllocExtra(Obj, size);
-  obj->next = heap->roots_to->next;
-  obj->prev = heap->roots_to;
+  obj->list.next = heap->roots_to.next;
+  obj->list.prev = &heap->roots_to;
   obj->space = heap->to_space;
   obj->refcount = 1;
-  heap->roots_to->next->prev = obj;
-  heap->roots_to->next = obj;
+  heap->roots_to.next->prev = &obj->list;
+  heap->roots_to.next = &obj->list;
   return obj->obj;
 }
 
@@ -287,21 +280,21 @@ void FbleRetainHeapObject(FbleHeap* heap, void* obj_)
     // This is a new root.
     if (obj->space == heap->to_space) {
       // We have already traversed the object, so move it to roots_to.
-      obj->prev->next = obj->next;
-      obj->next->prev = obj->prev;
-      obj->next = heap->roots_to->next;
-      obj->prev = heap->roots_to;
-      heap->roots_to->next->prev = obj;
-      heap->roots_to->next = obj;
+      obj->list.prev->next = obj->list.next;
+      obj->list.next->prev = obj->list.prev;
+      obj->list.next = heap->roots_to.next;
+      obj->list.prev = &heap->roots_to;
+      heap->roots_to.next->prev = &obj->list;
+      heap->roots_to.next = &obj->list;
     } else {
       // We haven't traversed the object yet, so move it to roots_from.
       // If the object was PENDING, we keep it marked as PENDING.
-      obj->prev->next = obj->next;
-      obj->next->prev = obj->prev;
-      obj->next = heap->roots_from->next;
-      obj->prev = heap->roots_from;
-      heap->roots_from->next->prev = obj;
-      heap->roots_from->next = obj;
+      obj->list.prev->next = obj->list.next;
+      obj->list.next->prev = obj->list.prev;
+      obj->list.next = heap->roots_from.next;
+      obj->list.prev = &heap->roots_from;
+      heap->roots_from.next->prev = &obj->list;
+      heap->roots_from.next = &obj->list;
     }
   }
 }
@@ -315,30 +308,30 @@ void FbleReleaseHeapObject(FbleHeap* heap, void* obj_)
     // This is no longer a root.
     if (obj->space == heap->to_space) {
       // We have already traversed this object. Move it to the "to" space.
-      obj->prev->next = obj->next;
-      obj->next->prev = obj->prev;
-      obj->next = heap->to->next;
-      obj->prev = heap->to;
-      heap->to->next->prev = obj;
-      heap->to->next = obj;
+      obj->list.prev->next = obj->list.next;
+      obj->list.next->prev = obj->list.prev;
+      obj->list.next = heap->to.next;
+      obj->list.prev = &heap->to;
+      heap->to.next->prev = &obj->list;
+      heap->to.next = &obj->list;
     } else if (obj->space == PENDING) {
       // We haven't traversed the object yet, but it's reachable from some
       // other root that we have already traversed. Move it to pending.
-      obj->prev->next = obj->next;
-      obj->next->prev = obj->prev;
-      obj->next = heap->pending->next;
-      obj->prev = heap->pending;
-      heap->pending->next->prev = obj;
-      heap->pending->next = obj;
+      obj->list.prev->next = obj->list.next;
+      obj->list.next->prev = obj->list.prev;
+      obj->list.next = heap->pending.next;
+      obj->list.prev = &heap->pending;
+      heap->pending.next->prev = &obj->list;
+      heap->pending.next = &obj->list;
     } else {
       // We haven't traversed the object yet, but it isn't reachable from
       // anything else we have traversed so far. Move it to "from" space.
-      obj->prev->next = obj->next;
-      obj->next->prev = obj->prev;
-      obj->next = heap->from->next;
-      obj->prev = heap->from;
-      heap->from->next->prev = obj;
-      heap->from->next = obj;
+      obj->list.prev->next = obj->list.next;
+      obj->list.next->prev = obj->list.prev;
+      obj->list.next = heap->from.next;
+      obj->list.prev = &heap->from;
+      heap->from.next->prev = &obj->list;
+      heap->from.next = &obj->list;
     }
   }
 }
@@ -358,12 +351,12 @@ void FbleHeapObjectAddRef(FbleHeap* heap, void* src_, void* dst_)
     // to move it to the list of pending objects.
     dst->space = PENDING;
     if (dst->refcount == 0) {
-      dst->prev->next = dst->next;
-      dst->next->prev = dst->prev;
-      dst->next = heap->pending->next;
-      dst->prev = heap->pending;
-      heap->pending->next->prev = dst;
-      heap->pending->next = dst;
+      dst->list.prev->next = dst->list.next;
+      dst->list.next->prev = dst->list.prev;
+      dst->list.next = heap->pending.next;
+      dst->list.prev = &heap->pending;
+      heap->pending.next->prev = &dst->list;
+      heap->pending.next = &dst->list;
     }
   }
 }
@@ -378,12 +371,12 @@ void FbleHeapRef(FbleHeap* heap, void* obj_)
     obj->space = PENDING;
     if (obj->refcount == 0) {
       // Non-root objects move to pending.
-      obj->prev->next = obj->next;
-      obj->next->prev = obj->prev;
-      obj->next = heap->pending->next;
-      obj->prev = heap->pending;
-      heap->pending->next->prev = obj;
-      heap->pending->next = obj;
+      obj->list.prev->next = obj->list.next;
+      obj->list.next->prev = obj->list.prev;
+      obj->list.next = heap->pending.next;
+      obj->list.prev = &heap->pending;
+      heap->pending.next->prev = &obj->list;
+      heap->pending.next = &obj->list;
     }
   }
 }
@@ -404,11 +397,11 @@ void FbleHeapFullGc(FbleHeap* heap)
     while (!IncrGc(heap));
 
     // Clean up all the free objects.
-    done = heap->free->next == heap->free;
-    while (heap->free->next != heap->free) {
-      Obj* obj = heap->free->next;
-      obj->prev->next = obj->next;
-      obj->next->prev = obj->prev;
+    done = heap->free.next == &heap->free;
+    while (heap->free.next != &heap->free) {
+      Obj* obj = (Obj*)heap->free.next;
+      obj->list.prev->next = obj->list.next;
+      obj->list.next->prev = obj->list.prev;
       heap->on_free(heap, obj->obj);
       FbleFree(obj);
     }
