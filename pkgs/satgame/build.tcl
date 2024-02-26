50ab1f635dbfde6984442f1641b5f2f8d0d0d2d7
Richard Uhler <ruhler@degralder.com>
2024-02-26 07:26:49 -0800

    Set up a test suite for sat game.
    The test is currently crashing.

--- pkgs/satgame/build.tcl
+++ pkgs/satgame/build.tcl
@@ -1,6 +1,11 @@
 namespace eval "pkgs/satgame" {
   pkg satgame [list core] ""
 
+  # SatGame/Tests compiled
+  stdio $::b/pkgs/satgame/satgame-tests "/SatGame/Tests%" "satgame"
+  testsuite $::b/pkgs/satgame/satgame-tests.tr $::b/pkgs/satgame/satgame-tests \
+    "$::b/pkgs/satgame/satgame-tests"
+
   # fble-satgame binary
   stdio $::b/pkgs/satgame/fble-satgame "/SatGame/Main%" "satgame"
   install $::b/pkgs/satgame/fble-satgame $::config::bindir/fble-satgame
