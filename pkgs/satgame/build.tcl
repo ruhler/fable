c1c8c1e9b86d4a7b822a5a4d1042cf521c339081
Richard Uhler <ruhler@degralder.com>
2024-03-08 07:19:10 -0800

    Set up screenshot app for satgame.
    To help with development of the graphics for the UI.

--- pkgs/satgame/build.tcl
+++ pkgs/satgame/build.tcl
@@ -1,12 +1,17 @@
 namespace eval "pkgs/satgame" {
-  pkg satgame [list core] ""
+  pkg satgame [list core app] ""
 
   # SatGame/Tests compiled
-  stdio $::b/pkgs/satgame/satgame-tests "/SatGame/Tests%" "satgame"
+  stdio $::b/pkgs/satgame/satgame-tests "/SatGame/Tests%" "satgame app"
   testsuite $::b/pkgs/satgame/satgame-tests.tr $::b/pkgs/satgame/satgame-tests \
     "$::b/pkgs/satgame/satgame-tests"
 
   # fble-satgame binary
-  stdio $::b/pkgs/satgame/fble-satgame "/SatGame/Main%" "satgame"
+  stdio $::b/pkgs/satgame/fble-satgame "/SatGame/Main%" "satgame app"
   install $::b/pkgs/satgame/fble-satgame $::config::bindir/fble-satgame
+
+  if $::config::enable_fble_app {
+    app $::b/pkgs/satgame/fble-satgame-screenshot "/SatGame/Screenshot/AppIO%" "satgame"
+    install $::b/pkgs/satgame/fble-satgame-screenshot $::config::bindir/fble-satgame-screenshot
+  }
 }
