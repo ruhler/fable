1c00041b0bd62f07c5935e94ea2535c2106cb0a4
Richard Uhler <ruhler@degralder.com>
2024-06-25 07:37:38 -0700

    Add fbld usage generated --help to pinball and graphics apps.

--- pkgs/graphics/build.tcl
+++ pkgs/graphics/build.tcl
@@ -1,6 +1,14 @@
 namespace eval "pkgs/graphics" {
   pkg graphics [list core app] ""
 
+  # Usage.fble is checked in from generated code. Add a test to ensure
+  # the checked in version is updated. If this test fails, update the source
+  # version of Usage.fble to match the generated version.
+  fbld_help_fble_usage $::b/pkgs/graphics/Graphics/Usage.fble $::s/pkgs/graphics/fble-graphics.fbld
+  test $::b/pkgs/graphics/Usage.tr \
+    "$::s/pkgs/graphics/Graphics/Usage.fble $::b/pkgs/graphics/Graphics/Usage.fble" \
+    "diff --strip-trailing-cr $::s/pkgs/graphics/Graphics/Usage.fble $::b/pkgs/graphics/Graphics/Usage.fble"
+
   # /Graphics/Tests% interpreted
   set cflags "-I $::s/pkgs/graphics -I $::s/pkgs/app -I $::s/pkgs/core"
   testsuite $::b/pkgs/graphics/tests-interpreted.tr "$::b/pkgs/core/fble-stdio $::b/pkgs/graphics/Graphics/Tests.fble.d" \
--- pkgs/pinball/build.tcl
+++ pkgs/pinball/build.tcl
@@ -1,6 +1,14 @@
 namespace eval "pkgs/pinball" {
   pkg pinball [list core app] ""
 
+  # Usage.fble is checked in from generated code. Add a test to ensure
+  # the checked in version is updated. If this test fails, update the source
+  # version of Usage.fble to match the generated version.
+  fbld_help_fble_usage $::b/pkgs/pinball/Pinball/Usage.fble $::s/pkgs/pinball/fble-pinball.fbld
+  test $::b/pkgs/pinball/Usage.tr \
+    "$::s/pkgs/pinball/Pinball/Usage.fble $::b/pkgs/pinball/Pinball/Usage.fble" \
+    "diff --strip-trailing-cr $::s/pkgs/pinball/Pinball/Usage.fble $::b/pkgs/pinball/Pinball/Usage.fble"
+
   # /Pinball/Tests% interpreted
   set cflags "-I $::s/pkgs/core -I $::s/pkgs/app -I $::s/pkgs/pinball"
   testsuite $::b/pkgs/pinball/tests-interpreted.tr "$::b/pkgs/core/fble-stdio $::b/pkgs/pinball/Pinball/Tests.fble.d" \
