f6ae015a22ecb6edc7ee25cd889b67f49c99cd46
Richard Uhler <ruhler@degralder.com>
2024-09-12 07:42:46 -0700

    Add some doc comments to fbld.h.

--- fbld/fbld.h
+++ fbld/fbld.h
@@ -9,19 +9,44 @@
 #include <stdbool.h>    // for bool
 #include <stddef.h>     // for size_t
 
-typedef struct FbldMarkup FbldMarkup;
-
+
+/**
+ * @struct[FbldLoc] Source file location
+ *  @field[FbleString*][source]
+ *   Source file name. Assumed to be owned by someone else.
+ *  @field[size_t][line] The line number, starting at 1.
+ *  @field[size_t][column] The column number, starting at 1.
+ */
 typedef struct {
   const char* file;
   size_t line;
   size_t column;
 } FbldLoc;
 
+/**
+ * @func[FbldError] Reports an error.
+ *  @arg[FbldLoc][loc] The location of the error.
+ *  @arg[const char*][msg] The error message.
+ *  @sideeffects
+ *   Prints the error message to stderr and aborts the program.
+ */
+void FbldError(FbldLoc loc, const char* message);
+
+/*
+ * @struct[FbldText] A string with a location.
+ *  @field[FbldLoc][loc] The source location of the string.
+ *  @field[char*][str] The contents of the string.
+ */
 typedef struct {
   FbldLoc loc;
   char str[];
 } FbldText;
 
+/**
+ * @struct[FbldTextV] Vector of FbldText.
+ *  @field[size_t][size] The number of elements.
+ *  @field[FbldText**][xs] The elements.
+ */
 typedef struct {
   size_t size;
   FbldText** xs;
@@ -36,22 +61,42 @@ typedef struct {
  *   Allocates new FbldText that should be freed when no longer needed.
  */
 FbldText* FbldNewText(FbldLoc loc, const char* str);
+
+/**
+ * Forward declaration for FbldMarkup.
+ */
+typedef struct FbldMarkup FbldMarkup;
 
+/**
+ * @struct[FbldMarkupV] A vector of FbldMarkup.
+ *  @field[size_t][size] Number of elements.
+ *  @field[FbldMarkup**][xs] The Elements.
+ */
 typedef struct {
   size_t size;
   FbldMarkup** xs;
 } FbldMarkupV;
 
+/**
+ * Different kinds of FbldMarkup.
+ */
 typedef enum {
   FBLD_MARKUP_PLAIN,
   FBLD_MARKUP_COMMAND,
   FBLD_MARKUP_SEQUENCE
 } FbldMarkupTag;
 
+/**
+ * @struct[FbldMarkup] Abstract syntax for fbld document.
+ *  @field[FbldMarkupTag][tag] The type of markup.
+ *  @field[FbldText*][text] Plain text, command name, or NULL.
+ *  @field[FbldMarkupV][markups] Sequence of markups, command args, or empty.
+ *  @field[size_t][refcount] Number of references to this markup.
+ */
 struct FbldMarkup {
   FbldMarkupTag tag;
-  FbldText* text;         // Plain text, command name, or NULL.
-  FbldMarkupV markups;    // Sequence of markups, command args, or empty.
+  FbldText* text;
+  FbldMarkupV markups;
   size_t refcount;
 };
 
@@ -72,48 +117,6 @@ void FbldFreeMarkup(FbldMarkup* markup);
  *   Allocates an FbldMarkup* that should be freed with FbldFreeMarkup.
  */
 FbldMarkup* FbldCopyMarkup(FbldMarkup* markup);
-
-/**
- * @func[FbldError] Reports an error.
- *  @arg[FbldLoc][loc] The location of the error.
- *  @arg[const char*][msg] The error message.
- *  @sideeffects
- *   Prints the error message to stderr and aborts the program.
- */
-void FbldError(FbldLoc loc, const char* message);
-
-/**
- * @func[FbldParse] Parses a sequence of fbld files.
- *  @arg[const char**][inputs] Null terminated array of input file names.
- *
- *  @returns[FbldMarkup*] The parsed markup.
- *
- *  @sideeffects
- *   @i Prints an error to stderr and aborts the program in case of error.
- *   @item
- *    References the input file names in the parsed markup. The caller is
- *    responsible for ensuring those pointers stay valid as long as they may
- *    continue to be access from the parsed markup.
- *   @item
- *    The user is responsible for calling FbldFreeMarkup on the returned
- *    markup object when no longer needed.
- */
-FbldMarkup* FbldParse(const char** inputs);
-
-/**
- * @func[FbldEval] Evaluates an fbld document.
- *  @arg[FbldMarkup*][markup] The markup to evaluate. Borrowed.
- *  @arg[bool][debug] Set to true to enable debug log output.
- *
- *  @returns[FbldMarkup*] The evaluated markup.
- *
- *  @sideeffects
- *   @i Prints an error to stderr and aborts the program in case of error.
- *   @item
- *    The user is responsible for calling FbldFreeMarkup on the returned
- *    markup object when no longer needed.
- */
-FbldMarkup* FbldEval(FbldMarkup* markup, bool debug);
 
 /**
  * @func[FbldMarkupLoc] Returns the location of the markup.
@@ -149,5 +152,38 @@ void FbldPrintMarkup(FbldMarkup* markup);
  *  @sideeffects Outputs the markup to stdout.
  */
 void FbldDebugMarkup(FbldMarkup* markup);
+
+/**
+ * @func[FbldParse] Parses a sequence of fbld files.
+ *  @arg[const char**][inputs] Null terminated array of input file names.
+ *
+ *  @returns[FbldMarkup*] The parsed markup.
+ *
+ *  @sideeffects
+ *   @i Prints an error to stderr and aborts the program in case of error.
+ *   @item
+ *    References the input file names in the parsed markup. The caller is
+ *    responsible for ensuring those pointers stay valid as long as they may
+ *    continue to be access from the parsed markup.
+ *   @item
+ *    The user is responsible for calling FbldFreeMarkup on the returned
+ *    markup object when no longer needed.
+ */
+FbldMarkup* FbldParse(const char** inputs);
+
+/**
+ * @func[FbldEval] Evaluates an fbld document.
+ *  @arg[FbldMarkup*][markup] The markup to evaluate. Borrowed.
+ *  @arg[bool][debug] Set to true to enable debug log output.
+ *
+ *  @returns[FbldMarkup*] The evaluated markup.
+ *
+ *  @sideeffects
+ *   @i Prints an error to stderr and aborts the program in case of error.
+ *   @item
+ *    The user is responsible for calling FbldFreeMarkup on the returned
+ *    markup object when no longer needed.
+ */
+FbldMarkup* FbldEval(FbldMarkup* markup, bool debug);
 
 #endif // _FBLD_H_
