c9abdf32cdc93c57832046d3058d1c9018e647cf
Richard Uhler <ruhler@degralder.com>
2024-10-23 07:37:25 -0700

    Draft first stage of a profile server.
    Starting at accepting connections. No http yet.

---
+++ scratch/profile_server/profile_server.c
@@ -0,0 +1,59 @@
+
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int sfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sfd < 0) {
+    perror("socket");
+    return 1;
+  }
+
+	struct sockaddr_in s_addr;
+	memset(&s_addr, 0, sizeof(s_addr));
+  s_addr.sin_family = AF_INET;
+  s_addr.sin_port = htons(8888);
+  s_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	if (bind(sfd, (struct sockaddr *) &s_addr, sizeof(s_addr)) < 0) {
+		perror("bind");
+    return 1;
+  }
+
+	if (listen(sfd, 10) < 0) {
+		perror("listen");
+    return 1;
+  }
+
+  while (true) {
+    int cfd = accept(sfd, NULL, NULL);
+    if (cfd < 0) {
+      perror("accept");
+      return 1;
+    }
+
+    char buf[1025];
+    ssize_t len = 0;
+    do {
+      for (size_t i = 0; i < len; ++i) {
+        printf("%c", toupper(buf[i]));
+      }
+      len = read(cfd, buf, 1024);
+      if (len < 0) {
+        perror("read");
+        return 1;
+      }
+    } while (len > 0);
+    close(cfd);
+  }
+
+  return 0;
+}
