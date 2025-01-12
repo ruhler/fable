11ec52b612e0c91fc9d7f3e4b0e37b046fb2db4f
Richard Uhler <ruhler@degralder.com>
2025-01-11 16:52:00 -0800

    Draft implementation of /Network/Sockets/IO/Builtin%.Client.
    It doesn't work. Needs debug.

--- pkgs/network/sockets.fble.c
+++ pkgs/network/sockets.fble.c
@@ -3,7 +3,13 @@
  *  Implementation of /Network/Sockets/IO/Builtin% module.
  */
 
-#include <assert.h>
+#define _POSIX_C_SOURCE 200112L  // for getaddrinfo
+
+#include <string.h>       // for memset
+#include <sys/types.h>    // for socket
+#include <sys/socket.h>   // for socket
+#include <netdb.h>        // for getaddrinfo
+#include <unistd.h>       // for read, write, close
 
 #include <fble/fble-function.h>
 #include <fble/fble-loc.h>
@@ -12,6 +18,25 @@
 #include <fble/fble-program.h>
 #include <fble/fble-string.h>
 #include <fble/fble-value.h>
+
+#include "int.fble.h"         // for FbleNewIntValue, FbleIntValueAccess
+#include "string.fble.h"      // for FbleNewStringValue, FbleStringValueAccess
+
+static void OnFree(void* data);
+static FbleValue* IStreamImpl(
+    FbleValueHeap* heap, FbleProfileThread* profile,
+    FbleFunction* function, FbleValue** args);
+static FbleValue* OStreamImpl(
+    FbleValueHeap* heap, FbleProfileThread* profile,
+    FbleFunction* function, FbleValue** args);
+static FbleValue* ClientImpl(
+    FbleValueHeap* heap, FbleProfileThread* profile,
+    FbleFunction* function, FbleValue** args);
+
+static FbleValue* IStream(FbleValueHeap* heap, FbleValue* sfd, FbleBlockId module_block_id);
+static FbleValue* OStream(FbleValueHeap* heap, FbleValue* sfd, FbleBlockId module_block_id);
+static FbleValue* Client(FbleValueHeap* heap, FbleBlockId module_block_id);
+static FbleValue* Server(FbleValueHeap* heap, FbleBlockId module_block_id);
 
 // -Wpedantic doesn't like our initialization of flexible array members when
 // defining static FbleString values.
@@ -25,13 +50,22 @@ static FbleString StrIO = { .refcount = 1, .magic = FBLE_STRING_MAGIC, .str = "I
 static FbleString StrBuiltin = { .refcount = 1, .magic = FBLE_STRING_MAGIC, .str = "Builtin", };
 
 static FbleString StrModuleBlock = { .refcount = 1, .magic = FBLE_STRING_MAGIC, .str = "/Network/Sockets/IO/Builtin%", };
+static FbleString StrClientBlock = { .refcount = 1, .magic = FBLE_STRING_MAGIC, .str = "/Network/Sockets/IO/Builtin%.Client", };
+static FbleString StrIStreamBlock = { .refcount = 1, .magic = FBLE_STRING_MAGIC, .str = "/Network/Sockets/IO/Builtin%.IStream", };
+static FbleString StrOStreamBlock = { .refcount = 1, .magic = FBLE_STRING_MAGIC, .str = "/Network/Sockets/IO/Builtin%.OStream", };
 
 #pragma GCC diagnostic pop
 
-#define NUM_PROFILE_BLOCKS 1
+#define CLIENT_BLOCK_OFFSET 1
+#define ISTREAM_BLOCK_OFFSET 2
+#define OSTREAM_BLOCK_OFFSET 3
+#define NUM_PROFILE_BLOCKS 4
 
 static FbleName ProfileBlocks[NUM_PROFILE_BLOCKS] = {
   { .name = &StrModuleBlock, .space = 0, .loc = { .source = &StrFile, .line = __LINE__, .col = 1 }},
+  { .name = &StrClientBlock, .space = 0, .loc = { .source = &StrFile, .line = __LINE__, .col = 1 }},
+  { .name = &StrIStreamBlock, .space = 0, .loc = { .source = &StrFile, .line = __LINE__, .col = 1 }},
+  { .name = &StrOStreamBlock, .space = 0, .loc = { .source = &StrFile, .line = __LINE__, .col = 1 }},
 };
 
 static FbleName PathEntries[] = {
@@ -48,10 +82,218 @@ static FbleModulePath Path = {
   .path = { .size = 4, .xs = PathEntries},
 };
 
+/**
+ * @func[OnFree] OnFree function for int fd native values.
+ *  @arg[void*][sfd] int fd to close.
+ *  @sideeffects
+ *   Closes the @a[sfd] socket.
+ */
+static void OnFree(void* data)
+{
+  int sfd = (int)(intptr_t)data;
+  close(sfd);
+}
+
+/**
+ * @func[IStreamImpl] FbleRunFunction to read a byte from a socket.
+ *  See documentation of FbleRunFunction in fble-function.h
+ *
+ *  The fble type of the function is @l{IO@<Maybe@<Int@>>}.
+ *
+ *  @sideeffects
+ *   Reads a byte from the socket.
+ */
+static FbleValue* IStreamImpl(
+    FbleValueHeap* heap, FbleProfileThread* profile,
+    FbleFunction* function, FbleValue** args)
+{
+  (void)profile;
+
+  int sfd = (int)(intptr_t)FbleNativeValueData(function->statics[0]);
+
+  FbleValue* world = args[0];
+
+  // TODO: Buffer the reads for better performance?
+  unsigned char c;
+  ssize_t r = read(sfd, &c, 1);
+
+  FbleValue* ms;
+  if (r == 0) {
+    ms = FbleNewEnumValue(heap, 1);
+  } else {
+    FbleValue* v = FbleNewIntValue(heap, c);
+    ms = FbleNewUnionValue(heap, 0, v);
+  }
+
+  return FbleNewStructValue_(heap, 2, world, ms);
+}
+
+/**
+ * @func[OStreamImpl] FbleRunFunction to write a byte to a file.
+ *  See documentation of FbleRunFunction in fble-function.h
+ *
+ *  The fble type of the function is @l{(Int@, World@) { R@<Unit@>; }}.
+ */
+static FbleValue* OStreamImpl(
+    FbleValueHeap* heap, FbleProfileThread* profile,
+    FbleFunction* function, FbleValue** args)
+{
+  (void)profile;
+
+  int sfd = (int)(intptr_t)FbleNativeValueData(function->statics[0]);
+
+  FbleValue* byte = args[0];
+  FbleValue* world = args[1];
+
+  // TODO: Buffer writes to improve performance?
+  unsigned char c = (unsigned char)FbleIntValueAccess(byte);
+  write(sfd, &c, 1);
+
+  FbleValue* unit = FbleNewStructValue_(heap, 0);
+  return FbleNewStructValue_(heap, 2, world, unit);
+}
+
+/**
+ * @func[IStream] Allocates an @l{IStream@} for a socket.
+ *  @arg[FbleValueHeap*][heap] The value heap.
+ *  @arg[FbleValue*][sfd] The socket to allocate the @l{IStream@} for.
+ *  @arg[FbleBlockId][module_block_id]
+ *   The block_id of the /Network/Sockets/IO/Builtin% block.
+ *
+ *  @returns[FbleValue*] An fble @l{IStream@} function value.
+ *
+ *  @sideeffects
+ *   Allocates a value on the heap.
+ */
+static FbleValue* IStream(FbleValueHeap* heap, FbleValue* sfd, FbleBlockId module_block_id)
+{
+  FbleExecutable exe = {
+    .num_args = 1,
+    .num_statics = 1,
+    .run = &IStreamImpl,
+  };
+  return FbleNewFuncValue(heap, &exe, module_block_id + ISTREAM_BLOCK_OFFSET, &sfd);
+}
+
+/**
+ * @func[OStream] Allocates an @l{OStream@} for a socket.
+ *  @arg[FbleValueHeap*][heap] The value heap.
+ *  @arg[FbleValue*][sfd] The socket to allocate the @l{OStream@} for.
+ *  @arg[FbleBlockId][module_block_id]
+ *   The block_id of the /Network/Sockets/IO/Builtin% block.
+ *
+ *  @returns[FbleValue*] An fble @l{OStream@} function value.
+ *
+ *  @sideeffects
+ *   Allocates a value on the heap.
+ */
+static FbleValue* OStream(FbleValueHeap* heap, FbleValue* sfd, FbleBlockId module_block_id)
+{
+  FbleExecutable exe = {
+    .num_args = 1,
+    .num_statics = 1,
+    .run = &OStreamImpl,
+  };
+  return FbleNewFuncValue(heap, &exe, module_block_id + OSTREAM_BLOCK_OFFSET, &sfd);
+}
+
+/**
+ * @func[ClientImpl] FbleRunFunction to do a tcp client connection.
+ *  See documentation of FbleRunFunction in fble-function.h
+ *
+ *  The fble type of the function is:
+ *
+ *  @code[fble] @
+ *   (String@, Int@, World@) { R@<Maybe@<IOStream@<IO@>>>; }
+ */
+static FbleValue* ClientImpl(
+    FbleValueHeap* heap, FbleProfileThread* profile,
+    FbleFunction* function, FbleValue** args)
+{
+  (void)profile;
+
+  char* host = FbleStringValueAccess(args[0]);
+  int64_t port = FbleIntValueAccess(args[1]);
+  FbleValue* world = args[2];
+
+  char portstr[10];
+  snprintf(portstr, 10, "%ld", port);
+
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = 0;
+  hints.ai_protocol = 0;
+
+  struct addrinfo* result = NULL;
+  getaddrinfo(host, portstr, &hints, &result);
+
+  int sfd = -1;
+  for (struct addrinfo* rp = result; sfd == -1 && rp != NULL; rp = rp->ai_next) {
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd < 0 && connect(sfd, rp->ai_addr, rp->ai_addrlen) == -1) {
+      sfd = -1;
+    }
+  }
+  freeaddrinfo(result);
+
+  FbleValue* mios;
+  if (sfd == -1) {
+    mios = FbleNewEnumValue(heap, 1); // Nothing
+  } else {
+    FbleBlockId module_block_id = function->profile_block_id - CLIENT_BLOCK_OFFSET;
+    FbleValue* sfd_value = FbleNewNativeValue(heap, (void*)(intptr_t)sfd, &OnFree);
+    FbleValue* istream = IStream(heap, sfd_value, module_block_id);
+    FbleValue* ostream = OStream(heap, sfd_value, module_block_id);
+    FbleValue* ios = FbleNewStructValue_(heap, 2, istream, ostream);
+    mios = FbleNewUnionValue(heap, 0, ios);  // Just(ios);
+  }
+
+  return FbleNewStructValue_(heap, 2, world, mios);
+}
+
+/**
+ * @func[Client] Allocates the Client function.
+ *  @arg[FbleValueHeap*][heap] The value heap.
+ *  @arg[FbleBlockId][module_block_id]
+ *   The block_id of the /Network/Sockets/IO/Builtin% block.
+ *  @returns[FbleValue*] The allocated function.
+ *  @sideeffects Allocates an fble value.
+ */
+static FbleValue* Client(FbleValueHeap* heap, FbleBlockId module_block_id)
+{
+  FbleExecutable exe = {
+    .num_args = 3,
+    .num_statics = 0,
+    .run = &ClientImpl,
+  };
+  return FbleNewFuncValue(heap, &exe, module_block_id + CLIENT_BLOCK_OFFSET, NULL);
+}
+
+/**
+ * @func[Server] Allocates the Server function.
+ *  @arg[FbleValueHeap*][heap] The value heap.
+ *  @arg[FbleBlockId][module_block_id]
+ *   The block_id of the /Network/Sockets/IO/Builtin% block.
+ *  @returns[FbleValue*] The allocated function.
+ *  @sideeffects Allocates an fble value.
+ */
+static FbleValue* Server(FbleValueHeap* heap, FbleBlockId module_block_id)
+{
+  // TODO: Implement me.
+  return NULL;
+}
+
 static FbleValue* Run(FbleValueHeap* heap, FbleProfileThread* profile, FbleFunction* function, FbleValue** args)
 {
-  assert(false && "TODO");
-  return NULL;
+  FbleBlockId module_block_id = function->profile_block_id;
+
+  FblePushFrame(heap);
+  FbleValue* client = Client(heap, module_block_id);
+  FbleValue* server = Server(heap, module_block_id);
+  FbleValue* sockets = FbleNewStructValue_(heap, 2, client, server);
+  return FblePopFrame(heap, sockets);
 }
 
 static FbleExecutable Executable = {
