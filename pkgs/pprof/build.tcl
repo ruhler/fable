6b930fa2f7ad368419edfc6b184af6e1cc66f29a
Richard Uhler <ruhler@degralder.com>
2025-01-20 12:12:42 -0800

    Add test to pprof project.

--- pkgs/pprof/build.tcl
+++ pkgs/pprof/build.tcl
@@ -1,6 +1,11 @@
 namespace eval "pkgs/pprof" {
   pkg pprof [list core network] "" ""
 
+  # Pprof/Tests compiled
+  stdio $::b/pkgs/pprof/pprof-tests "/Pprof/Tests%" "network pprof" ""
+  testsuite $::b/pkgs/pprof/pprof-tests.tr $::b/pkgs/pprof/pprof-tests \
+    "$::b/pkgs/pprof/pprof-tests"
+
   # fble-pprof program.
   stdio $::b/pkgs/pprof/fble-pprof "/Pprof/Server/Main%" "network pprof" ""
   install $::b/pkgs/pprof/fble-pprof $::config::bindir/fble-pprof
