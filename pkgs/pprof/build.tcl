3dd9f9142ec7bfc3b84c4cff8571884037a16b5f
Richard Uhler <ruhler@degralder.com>
2025-02-07 18:16:54 -0800

    Add missing sockets library on windows.

--- pkgs/pprof/build.tcl
+++ pkgs/pprof/build.tcl
@@ -6,8 +6,14 @@ namespace eval "pkgs/pprof" {
   testsuite $::b/pkgs/pprof/pprof-tests.tr $::b/pkgs/pprof/pprof-tests \
     "$::b/pkgs/pprof/pprof-tests"
 
+  set ldflags ""
+  if {[string first "_NT" [exec uname -s]] != -1} {
+    # Windows needs to link to Ws2_32.lib for the sockets library.
+    set ldflags "-lWs2_32"
+  }
+
   # fble-pprof program.
-  stdio $::b/pkgs/pprof/fble-pprof "/Pprof/Server/Main%" "network pprof" ""
+  stdio $::b/pkgs/pprof/fble-pprof "/Pprof/Server/Main%" "network pprof" "$ldflags"
   install $::b/pkgs/pprof/fble-pprof $::config::bindir/fble-pprof
 
   # fble-pprof-report program.
