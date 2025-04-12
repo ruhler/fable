8df5b9af8c52dfeef7c1d1e2df829ac5e08f5d07
Richard Uhler <ruhler@degralder.com>
2025-04-12 11:11:54 -0700

    Read pprof samples in correct order.

--- pkgs/pprof/pprof.py
+++ pkgs/pprof/pprof.py
@@ -3,38 +3,38 @@ import sys
 
 class Node:
     def __init__(self, name):
-        self.total = 0
         self.self = 0
         self.name = name
         self.children = {}
                      
 
 def Traverse(node, entry):
-    node.total += 1
     if entry in node.children:
         child = node.children[entry]
-        child.total += 1
         return child
     child = Node(entry)
-    child.total += 1
     node.children[entry] = child
     return child
 
 def Dump(node, indent):
-    print("%s%s %d %d" % (indent, node.name, node.total, node.self))
+    print("%s%s %d" % (indent, node.name, node.self))
     for entry in node.children:
         Dump(node.children[entry], indent + "  ")
 
 root = Node("<root>")
 
-node = root
+samples = []
 for line in sys.stdin:
     if line.startswith('\t'):
         [addr, name, so] = line.split()
         entry = name.split('+')[0]
-        node = Traverse(node, entry)
+        samples.append(entry)
     elif len(line.strip()) == 0:
-        node.self += 1
+        samples.reverse()
         node = root
+        for entry in samples:
+            node = Traverse(node, entry)
+        node.self += 1
+        samples = []
 
 Dump(root, "")
