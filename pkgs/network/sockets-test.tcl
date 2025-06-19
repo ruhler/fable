c8d6ad2fe9e4e7877393f51f0b30fd72a00db086
Richard Uhler <ruhler@degralder.com>
2025-06-18 19:24:45 -0700

    Try to get rid of occasional deadlock in sockets-test.

--- pkgs/network/sockets-test.tcl
+++ pkgs/network/sockets-test.tcl
@@ -12,12 +12,22 @@ set echo [lindex $argv 0]
 set client [lindex $argv 1]
 
 puts "Launching $echo..."
-exec $echo -p 9007 -n 1 &
+set server_pid [exec $echo -p 9007 -n 1 &]
 
-puts "Launching $client..."
-set output [exec $client -p 9007 -m hello -n 5]
+try {
+  # Give some time for the server to launch before launching the client to
+  # reduce the chance of the client racing ahead.
+  after 1000
 
-if {![string equal $output "hello"]} {
-  error "expected hello, got $output"
+  puts "Launching $client..."
+  set output [exec $client -p 9007 -m hello -n 5]
+
+  if {![string equal $output "hello"]} {
+    error "expected hello, got $output"
+  }
+  exit 0
+} finally {
+  # Prevent deadlock in case the client never manages to connect with the
+  # server.
+  exec kill $server_pid
 }
-exit 0
