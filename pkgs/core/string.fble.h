0af710a925dfa0932fb76a49b8b941b2357bce38
Richard Uhler <ruhler@degralder.com>
2025-10-14 07:42:35 -0700

    Move /Core/String%, /Core/String/Eq%, and /Core/String/Lines% to /Std.

--- pkgs/core/string.fble.h
+++ pkgs/core/string.fble.h
@@ -8,12 +8,12 @@
 #include <fble/fble-value.h>
 
 /**
- * @func[FbleNewStringValue] Converts a C string to @l{/Core/String%.String@}.
+ * @func[FbleNewStringValue] Converts a C string to @l{/Std/String%.String@}.
  *  @arg[FbleValueHeap*][heap] The heap to use for allocations.
  *  @arg[const char*][str] The string to convert.
  *
  *  @returns[FbleValue*]
- *   A newly allocated fble @l{/Core/String%.String@} with the contents of str.
+ *   A newly allocated fble @l{/Std/String%.String@} with the contents of str.
  *
  *  @sideeffects
  *   Allocates an FbleValue on the heap.
@@ -22,7 +22,7 @@ FbleValue* FbleNewStringValue(FbleValueHeap* heap, const char* str);
 
 /**
  * @func[FbleStringValueAccess] Convert a @l{String@} value to a c string.
- *  @arg[FbleValue*][str] The @l{/Core/String%.String@} to convert.
+ *  @arg[FbleValue*][str] The @l{/Std/String%.String@} to convert.
  *
  *  @returns[char*]
  *   A newly allocated nul terminated c string with the contents of str.
@@ -34,8 +34,8 @@ FbleValue* FbleNewStringValue(FbleValueHeap* heap, const char* str);
 char* FbleStringValueAccess(FbleValue* str);
 
 /**
- * @func[FbleDebugTrace] Prints a @l{/Core/String%.String@} value to stderr.
- *  @arg[FbleValue*][str] The @l{/Core/String%.String@} to print.
+ * @func[FbleDebugTrace] Prints a @l{/Std/String%.String@} value to stderr.
+ *  @arg[FbleValue*][str] The @l{/Std/String%.String@} to print.
  *
  *  @sideeffects
  *   Prints the string to stderr.
