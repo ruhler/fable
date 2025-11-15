8e26284d21ea6bd763b49a37e29b771dc3084c44
Richard Uhler <ruhler@degralder.com>
2025-11-15 07:14:17 -0800

    Set up infra for tests.

--- pkgs/computrainer/build.tcl
+++ pkgs/computrainer/build.tcl
@@ -1,6 +1,13 @@
 namespace eval "pkgs/computrainer" {
   pkg computrainer [list core app] "" ""
 
+  # Computrainer/Tests compiled
+  # --allow-shlib-undefined because we know this doesn't use the part of the
+  # app package that depends on SDL.
+  stdio $::b/pkgs/computrainer/computrainer-tests "/Computrainer/Tests%" "app computrainer" "-Wl,--allow-shlib-undefined"
+  testsuite $::b/pkgs/computrainer/computrainer-tests.tr $::b/pkgs/computrainer/computrainer-tests \
+    "$::b/pkgs/computrainer/computrainer-tests"
+
   if $::config::enable_fble_app {
     app $::b/pkgs/computrainer/computrainer-bin "/Computrainer/Main%" "computrainer"
     install $::b/pkgs/computrainer/computrainer-bin $::config::bindir/computrainer
