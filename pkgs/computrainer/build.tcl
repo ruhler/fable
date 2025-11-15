8e2cec35223a75c6f98be310cf4c2843eca4a31b
Richard Uhler <ruhler@degralder.com>
2025-11-14 19:16:00 -0800

    Move main program to /Computrainer/Main%.

--- pkgs/computrainer/build.tcl
+++ pkgs/computrainer/build.tcl
@@ -2,7 +2,7 @@ namespace eval "pkgs/computrainer" {
   pkg computrainer [list core app] "" ""
 
   if $::config::enable_fble_app {
-    app $::b/pkgs/computrainer/computrainer "/Computrainer%" "computrainer"
-    install $::b/pkgs/computrainer/computrainer $::config::bindir/computrainer
+    app $::b/pkgs/computrainer/computrainer-bin "/Computrainer/Main%" "computrainer"
+    install $::b/pkgs/computrainer/computrainer-bin $::config::bindir/computrainer
   }
 }
