fa344c59af22e88a4727fc00f504d868363e47d8
Richard Uhler <ruhler@degralder.com>
2026-05-30 12:38:13 -0700

    Use a more compact encoding for literal values.
    Super long literal value smashes the stack. Will fix shortly.

--- lib/literal.c
+++ lib/literal.c
@@ -5,6 +5,7 @@
 
 #include "literal.h"
 
+#include <assert.h>   // for assert
 #include <string.h>   // for strlen, strncmp
 
 #include <fble/fble-vector.h>   // for FbleInitVector, FbleFreeVector
@@ -13,19 +14,24 @@
 
 // The encoding for literal values is a mini program.
 //
-// The program constructs a literal starting with a unit value and empty
-// list, then executing words from the program in reverse order as follows:
+// The program constructs a literal starting with a Unit value and empty
+// list, then interpreting bytes from the program in order as follows:
 //
-// If the word is -1, prepends the current value to the list and sets the
-// current value to unit. Otherwise the word is treated as the tag width of
-// a union value, the next word is treated as the tag value of a union
-// value, and the value is set to the union value with given tag width, tag
-// value, and previous value as the argument.
+// First byte is [WWWWWWW, L]:
+//  * The most least significant bit L is set to 1 to indicate this is the
+//    last tag value before adding the element to the list and resetting the
+//    argument to Unit. The bit L is set to 0 to indicate the value needs to
+//    be wrapped by more tag values before adding it to the list.
+//  * The most significant 7 bits WWWWWWW specifies the tag width of the union
+//    value to wrap the current argument value in.
+//
+// Followed by as few bytes as needed to encode the tag value given the tag
+// width, most significant byte to least significant byte.
 
 typedef struct {
   size_t size;
-  size_t* xs;
-} FbleTagV;
+  uint8_t* xs;
+} ByteV;
 
 /**
  * @struct[TypeList] Linked list of types to detect recursion.
@@ -37,30 +43,61 @@ typedef struct TypeList {
   struct TypeList* next;
 } TypeList;
 
-static size_t GetNextLetter(FbleTypeHeap* th, FbleType* type, const char* word, FbleTagV* letter, TypeList* tl);
+static void AppendTag(size_t tagwidth, size_t tag, bool last, ByteV* data);
+static const char* GetNextLetter(FbleTypeHeap* th, FbleType* type, const char* word, ByteV* data, TypeList* tl);
+static const char* ParseLiteral(FbleTypeHeap* th, FbleType* type, const char* word, ByteV* data);
+
+/**
+ * @func[AppendTag] Appends an encoded tag header and value to data stream.
+ *  @arg[size_t][tagwidth] The width of the tag.
+ *  @arg[size_t][tag] The tag value.
+ *  @arg[bool][last] True if this is the last tag to add for an element.
+ *  @arg[ByteV*][data] The data to append to.
+ *  @sideeffects Appends the tag value encoded to the data stream.
+ */
+static void AppendTag(size_t tagwidth, size_t tag, bool last, ByteV* data)
+{
+  uint8_t header = tagwidth << 1;
+  if (last) {
+    header |= 0x1;
+  }
+  FbleAppendToVector(*data, header);
+
+  // Encoding is minimum number of bytes for the given tag width, most
+  // significant byte of the tag first.
+  if (tagwidth > 24) {
+    FbleAppendToVector(*data, (tag >> 24) & 0xFF);
+  }
+  if (tagwidth > 16) {
+    FbleAppendToVector(*data, (tag >> 16) & 0xFF);
+  }
+  if (tagwidth > 8) {
+    FbleAppendToVector(*data, (tag >> 8) & 0xFF);
+  }
+  if (tagwidth > 0) {
+    FbleAppendToVector(*data, (tag >> 0) & 0xFF);
+  }
+}
 
 /**
  * @func[GetNextLetter] Finds the next letter value for a literal.
  *  @arg[FbleTypeHeap*][th] The type heap
  *  @arg[FbleType*][type] The letter type.
  *  @arg[const char*][word] The literal word to get the next letter in.
- *  @arg[FbleTagV*][letter]
- *   Output where the matched letter will be placed. This is interpreted as a
- *   sequence of (tagwidth, tag) pairs corresponding to a FbleNewLiteralValue
- *   prgm. For example, if the runtime value for the letter to construct
- *   is @code[txt]{6@(3: 2@(1: @()))}, the sequence @code[txt]{2, 1, 6, 3}.
+ *  @arg[TagV*][data]
+ *   Output where the matched letter will be placed in literal data format.
  *  @arg[TypeList*][tl] List of type args on stack to detect recursion
- *  @returns[size_t]
- *   The number of chars in word that were matched. 0 if no match was found.
- *  @sideeffects Appends tags to letter if a match was found.
+ *  @returns[const char*]
+ *   The rest of the word after the next letter. NULL if no match was found.
+ *  @sideeffects Writes literal data for the letter to data if a match was found.
  */
-static size_t GetNextLetter(FbleTypeHeap* th, FbleType* type, const char* word, FbleTagV* letter, TypeList* tl)
+static const char* GetNextLetter(FbleTypeHeap* th, FbleType* type, const char* word, ByteV* data, TypeList* tl)
 {
   FbleDataType* dt = (FbleDataType*)FbleNormalType(th, type);
   if (dt->_base.tag != FBLE_DATA_TYPE || dt->datatype != FBLE_UNION_DATATYPE) {
     // Letters can only be found in union types.
     FbleReleaseType(th, &dt->_base);
-    return 0;
+    return NULL;
   }
 
   for (TypeList* l = tl; l != NULL; l = l->next) {
@@ -68,95 +105,124 @@ static size_t GetNextLetter(FbleTypeHeap* th, FbleType* type, const char* word, 
       // We've circled back recursively to a type we've already started
       // searching in. There's nothing new we'll find this time around.
       FbleReleaseType(th, &dt->_base);
-      return 0;
+      return NULL;
     }
   }
 
+  // If we have a null type list, it means we are out the outer most layer of
+  // union types, so this will be the last tag we need before adding the
+  // argument to the list of literal elements.
+  bool last = (tl == NULL);
+  size_t tagwidth = FbleTagWidth(dt->fields.size);
+
   for (size_t i = 0; i < dt->fields.size; ++i) {
+    // Match letter against a Unit field.
     if (FbleIsUnitType(th, dt->fields.xs[i].type)) {
-      // See if we found our letter.
       const char* fieldname = dt->fields.xs[i].name.name->str;
       size_t len = strlen(fieldname);
       if (len > 0 && strncmp(word, fieldname, len) == 0) {
-        FbleAppendToVector(*letter, FbleTagWidth(dt->fields.size));
-        FbleAppendToVector(*letter, i);
+        AppendTag(tagwidth, i, last, data);
         FbleReleaseType(th, &dt->_base);
-        return len;
+        return word + len;
       }
     }
 
+    // Search for letter in a non-Unit subfield.
     TypeList ntl = { .type = &dt->_base, .next = tl };
-    size_t sub = GetNextLetter(th, dt->fields.xs[i].type, word, letter, &ntl);
-
-    if (sub > 0) {
-      FbleAppendToVector(*letter, FbleTagWidth(dt->fields.size));
-      FbleAppendToVector(*letter, i);
+    const char* next = GetNextLetter(th, dt->fields.xs[i].type, word, data, &ntl);
+    if (next != NULL) {
+      AppendTag(tagwidth, i, last, data);
       FbleReleaseType(th, &dt->_base);
-      return sub;
+      return next;
     }
   }
 
   FbleReleaseType(th, &dt->_base);
-  return 0;
+  return NULL;
+}
+
+/**
+ * @func[ParseLiteral] Parse the rest of a literal.
+ *  @arg[FbleTypeHeap*][th] The type heap
+ *  @arg[FbleType*][type] The letter type.
+ *  @arg[const char*][word] The literal word to parse.
+ *  @arg[TagV*][data] Output for parsed literal data.
+ *  @returns[const char*]
+ *   NULL on success. Otherwise points to where in the literal an error was
+ *   found.
+ *  @sideeffects Appends literal data to data.
+ */
+static const char* ParseLiteral(FbleTypeHeap* th, FbleType* type, const char* word, ByteV* data)
+{
+  if (*word == '\0') {
+    return NULL;  // Nothing to parse. Return success.
+  }
+
+  ByteV letter;
+  FbleInitVector(letter);
+
+  const char* next = GetNextLetter(th, type, word, &letter, NULL);
+  if (next == NULL) {
+    FbleFreeVector(letter);
+    return word;  // error at this location
+  }
+
+  const char* err = ParseLiteral(th, type, next, data);
+  if (err != NULL) {
+    FbleFreeVector(letter);
+    return err;   // propagate the error
+  }
+
+  for (size_t i = 0; i < letter.size; ++i) {
+    FbleAppendToVector(*data, letter.xs[i]);
+  }
+  FbleFreeVector(letter);
+  return NULL;    // success
 }
 
 FbleLiteral FbleParseLiteral(FbleTypeHeap* th, FbleType* type, const char* word)
 {
-  const char* start = word;
-  size_t wordlen = strlen(word);
-
-  FbleTagV data;
+  ByteV data;
   FbleInitVector(data);
 
-  FbleTagV letter;
-  FbleInitVector(letter);
-
-  while (wordlen > 0) {
-    letter.size = 0;
-    size_t len = GetNextLetter(th, type, word, &letter, NULL);
-
-    if (len == 0) {
-      FbleFreeVector(data);
-      FbleFreeVector(letter);
-      FbleLiteral failed = { .size = word - start, .data = NULL };
-      return failed;
-    }
-    
-    FbleAppendToVector(data, -1);
-    for (size_t i = 0; i < letter.size; ++i) {
-      size_t tag = letter.xs[letter.size - i - 1];
-      FbleAppendToVector(data, tag);
-    }
-
-    word += len;
-    wordlen -= len;
+  const char* err = ParseLiteral(th, type, word, &data);
+  if (err != NULL) {
+    FbleFreeVector(data);
+    FbleLiteral failed = { .size = err - word, .data = NULL };
+    return failed;
   }
 
-  FbleFreeVector(letter);
-
-  FbleLiteral literal = { .size = sizeof(size_t) * data.size, .data = (uint8_t*)data.xs };
+  FbleLiteral literal = { .size = data.size, .data = data.xs };
+  // fprintf(stderr, "%zi\n", literal.size);
   return literal;
 }
 
 // See documentation in fble-literal.h.
-FbleValue* FbleNewLiteralValue(FbleValueHeap* heap, size_t size, uint8_t* bytes)
+FbleValue* FbleNewLiteralValue(FbleValueHeap* heap, size_t size, uint8_t* data)
 {
-  size_t* data = (size_t*)bytes;
-  size /= sizeof(size_t);
+  uint8_t* end = data + size;
 
   FbleValue* unit = FbleNewStructValue_(heap, 0);
-  FbleValue* tail = FbleNewUnionValue(heap, 1, 1, unit);
-  FbleValue* letter = unit;
-  for (size_t* pc = data + size - 1; pc >= data; --pc) {
-    if (*pc == -1) {
-      FbleValue* cons = FbleNewStructValue_(heap, 2, letter, tail);
-      tail = FbleNewUnionValue(heap, 1, 0, cons);
-    } else {
-      size_t tagwidth = *pc;
-      pc--;
-      size_t tag = *pc;
-      letter = FbleNewUnionValue(heap, tagwidth, tag, letter);
+  FbleValue* list = FbleNewUnionValue(heap, 1, 1, unit);
+  FbleValue* arg = unit;
+  while (data < end) {
+    uint8_t header = *data++;
+    size_t tagwidth = header >> 1;
+    bool last = header & 0x1;
+
+    size_t tag = 0;
+    for (size_t i = 0; 8 * i < tagwidth; i++) {
+      assert(data < end && "malformed literal data");
+      tag <<= 8;
+      tag |= *data++;
+    }
+
+    arg = FbleNewUnionValue(heap, tagwidth, tag, arg);
+    if (last) {
+      FbleValue* cons = FbleNewStructValue_(heap, 2, arg, list);
+      list = FbleNewUnionValue(heap, 1, 0, cons);
+      arg = unit;
     }
   }
-  return tail;
+  return list;
 }
